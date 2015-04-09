6756d0e758f93f1a87004fce8e7afccf61212f69
Trevor Rowbotham <smokey101stair@gmail.com>
2015-04-09 14:10:10 -0400

    Add some missing property names to HTMLElement

--- HTMLElement/HTMLElement.class.php
+++ HTMLElement/HTMLElement.class.php
@@ -6,11 +6,15 @@ class HTMLElement extends Element {
 	protected $mAccessKey;
 	protected $mAccessKeyLabel;
 	protected $mContentEditable;
+	protected $mDir;
 	protected $mDataset;
+	protected $mHidden;
 	protected $mIsContentEditable;
 	protected $mLang;
+	protected $mSpellcheck;
 	protected $mTabIndex;
 	protected $mTitle;
+	protected $mTranslate;
 
 	protected function __construct() {
 		parent::__construct();
@@ -19,11 +23,15 @@ class HTMLElement extends Element {
 		$this->mAccessKeyLabel = '';
 		$this->mContentEditable = false;
 		$this->mDataset;
+		$this->mDir = '';
+		$this->mHidden = false;
 		$this->mIsContentEditable = false;
 		$this->mLang = '';
 		$this->mNodeType = Node::ELEMENT_NODE;
+		$this->mSpellcheck = false;
 		$this->mTabIndex = '';
 		$this->mTitle = '';
+		$this->mTranslate = false;
 	}
 
 	public function __get($aName) {
@@ -36,14 +44,22 @@ class HTMLElement extends Element {
 				return $this->mContentEditable;
 			case 'dataset':
 				return $this->mDataset;
+			case 'dir':
+				return $this->mDir;
+			case 'hidden':
+				return $this->mHidden;
 			case 'isContentEditable':
 				return $this->mIsContentEditable;
 			case 'lang':
 				return $this->mLang;
+			case 'spellcheck':
+				return $this->mSpellcheck;
 			case 'tabIndex':
 				return $this->mTabIndex;
 			case 'title':
 				return $this->mTitle;
+			case 'translate':
+				return $this->mTranslate;
 			default:
 				return parent::__get($aName);
 		}
