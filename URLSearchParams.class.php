f11f453e63dfed524f053b25fd826450297251a6
Trevor Rowbotham <smokey101stair@gmail.com>
2015-04-11 02:44:47 -0400

    Document URLSearchParams

--- URLSearchParams.class.php
+++ URLSearchParams.class.php
@@ -17,27 +17,59 @@ class URLSearchParams {
 		}
 	}
 
+	/**
+	 * Appends a new key -> value pair to the end of the query string.
+	 * @param  string $aName  The name of the key in the pair.
+	 * @param  string $aValue The value assigned to the key.
+	 */
 	public function append($aName, $aValue) {
 		$this->mIndex[$this->mSequenceId] = $aName;
 		$this->mParams[$aName][$this->mSequenceId++] = $aValue;
 	}
 
+	/**
+	 * Deletes all occurances of pairs with the specified key name.
+	 * @param  string $aName The name of the key to delete.
+	 */
 	public function delete($aName) {
 		unset($this->mParams[$aName]);
 	}
 
+	/**
+	 * Get the value of the first key -> value pair with the specified key name.
+	 * @param  string $aName The name of the key whose value you want to retrive.
+	 * @return string        The value of the specified key.
+	 */
 	public function get($aName) {
 		return $this->has($aName) ? reset($this->mParams[$aName]) : null;
 	}
 
+	/**
+	 * Gets all key -> value pairs that has the specified key name.
+	 * @param  string $aName The name of the key whose values you want to retrieve.
+	 * @return array         An array containing all the values of the specified key.
+	 */
 	public function getAll($aName) {
 		return $this->has($aName) ? array_values($this->mParams[$aName]) : array();
 	}
 
+	/**
+	 * Indicates whether or not a query string contains any keys with the specified key name.
+	 * @param  boolean  $aName The key name you want to test if it exists.
+	 * @return boolean         Returns true if the key exits, otherwise false.
+	 */
 	public function has($aName) {
 		return isset($this->mParams[$aName]);
 	}
 
+	/**
+	 * Sets the value of the specified key name.  If multiple pairs exist with the same key name
+	 * it will set the value for the first occurance of the key in the query string and all other
+	 * occurances will be removed from the query string.  If the key does not already exist in the
+	 * query string, it will be added to the end of the query string.
+	 * @param string $aName  The name of the key you want to modify the value of.
+	 * @param string $aValue The value you want to associate with the key name.
+	 */
 	public function set($aName, $aValue) {
 		if ($this->has($aName)) {
 			for ($i = count($this->mParams[$aName]) - 1; $i > 0; $i--) {
@@ -53,6 +85,10 @@ class URLSearchParams {
 		}
 	}
 
+	/**
+	 * Returns all key -> value pairs stringified in the correct order.
+	 * @return string The query string.
+	 */
 	public function toString() {
 		$queryString = '';
 
