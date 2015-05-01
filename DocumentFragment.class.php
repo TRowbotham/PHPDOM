4cf0e1b1f1fe332f6ed38c021d492559689fff8a
Trevor Rowbotham <smokey101stair@gmail.com>
2015-05-01 02:05:24 -0400

    Add nodeName to DocumentFragment

--- DocumentFragment.class.php
+++ DocumentFragment.class.php
@@ -3,6 +3,7 @@ class DocumentFragment extends Node {
 	public function __construct() {
 		parent::__construct();
 
+		$this->mNodeName = '#document-fragment';
 		$this->mNodeType = Node::DOCUMENT_FRAGMENT_NODE;
 	}
 }
