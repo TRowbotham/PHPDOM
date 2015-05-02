1a61bafb9e6fb8c4e3153598426642421bd60628
Trevor Rowbotham <smokey101stair@gmail.com>
2015-05-01 22:11:08 -0400

    Add support for the wholeText property

--- Text.class.php
+++ Text.class.php
@@ -5,20 +5,30 @@
 require_once 'CharacterData.class.php';
 
 class Text extends CharacterData {
-	private $mWholeText;
-
 	public function __construct($aData = '') {
 		parent::__construct();
 
 		$this->mNodeName = '#text';
 		$this->mNodeType = Node::TEXT_NODE;
-		$this->mWholeText = $aData;
+		$this->mData = $aData;
+		$this->mLength = strlen($aData);
 	}
 
 	public function __get($aName) {
 		switch ($aName) {
 			case 'wholeText':
-				return $this->mWholeText;
+				$wholeText = '';
+
+				if ($this->parentNode) {
+					foreach ($this->parentNode->childNodes as $node) {
+						if ($node instanceof Text) {
+							$wholeText += $node->data;
+						}
+					}
+				}
+
+				return $wholeText;
+
 			default:
 				return parent::__get($aName);
 		}
