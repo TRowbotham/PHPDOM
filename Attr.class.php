fcec11a94e04b84680b174bf4fac6f4ea879cfdd
Trevor Rowbotham <smokey101stair@gmail.com>
2015-05-03 00:17:23 -0400

    Better align with the spec

--- Attr.class.php
+++ Attr.class.php
@@ -5,24 +5,33 @@
 require_once 'Node.class.php';
 
 class Attr extends Node {
+	protected $mLocalName;
+	protected $mName;
 	protected $mNamespaceURI;
 	protected $mPrefix;
 	protected $mSpecified;
+	protected $mValue;
 
-	public function __construct() {
+	public function __construct($aName) {
 		parent::__construct();
 
+		$this->mLocalName = $aName;
+		$this->mName = $aName;
+		$this->mNodeName = $aName;
 		$this->mNamespaceURI = null;
 		$this->mNodeType = Node::ATTRIBUTE_NODE;
 		$this->mPrefix = null;
-		$this->mSpecified = false;
+		$this->mSpecified = true;
+		$this->mValue = '';
 	}
 
 	public function __get($aName) {
 		switch ($aName) {
 			case 'localName':
+				return $this->mLocalName;
+
 			case 'name':
-				return $this->mNodeName;
+				return $this->mName;
 
 			case 'namespaceURI':
 				return $this->mNamespaceURI;
@@ -34,7 +43,26 @@ class Attr extends Node {
 				return $this->mSpecified;
 
 			case 'value':
-				return $this->mNodeValue;
+				return $this->mValue;
+
+			default:
+				return parent::__get($aName);
+		}
+	}
+
+	public function __set($aName, $aValue) {
+		switch ($aName) {
+			case 'nodeValue':
+			case 'textContent':
+			case 'value':
+				$this->mNodeValue = $aValue;
+				$this->mTextContent = $aValue;
+				$this->mValue = $aValue;
+
+				break;
+
+			default:
+				parent::__get($aName);
 		}
 	}
 
