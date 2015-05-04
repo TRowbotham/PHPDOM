f64aac6443da14b6a3e2e9842a05d7aa1df2174a
Trevor Rowbotham <smokey101stair@gmail.com>
2015-05-03 20:52:05 -0400

    Implement ParentNode

--- DocumentFragment.class.php
+++ DocumentFragment.class.php
@@ -2,7 +2,11 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/DocumentFragment
 // https://dom.spec.whatwg.org/#interface-documentfragment
 
+require_once 'ParentNode.class.php';
+
 class DocumentFragment extends Node {
+	use ParentNode;
+
 	public function __construct() {
 		parent::__construct();
 
