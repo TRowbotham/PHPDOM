6d4ce2c159f1885a991ede7b8c2d4fd523abbde5
Trevor Rowbotham <smokey101stair@gmail.com>
2015-05-05 17:45:53 -0400

    Add documentation

--- DOMTokenList.class.php
+++ DOMTokenList.class.php
@@ -20,6 +20,14 @@ class DOMTokenList implements ArrayAccess, SplSubject {
 		}
 	}
 
+	/**
+	 * Adds all arguments to the token list except for those that
+	 * already exist in the list.  A SyntaxError will be thrown if
+	 * one of the tokens is an empty string and a InvalidCharacterError
+	 * will be thrown if one of the tokens contains ASCII whitespace.
+	 * @param string $aTokens... One or more tokens to be added to the token
+	 *                           list.
+	 */
 	public function add($aTokens) {
 		if (!func_num_args()) {
 			return;
@@ -38,20 +46,44 @@ class DOMTokenList implements ArrayAccess, SplSubject {
 		$this->notify();
 	}
 
+	/**
+	 * Attach an observer to this object to be notified when changes occur.
+	 * @param  SplObserver $aObserver The object to be notified of changes.
+	 */
 	public function attach(SplObserver $aObserver) {
 		$this->mObservers->attach($aObserver);
 	}
 
+	/**
+	 * Returns true if the token is present, and false otherwise.
+	 * A SyntaxError will be thrown if one of the tokens is an empty string
+	 * and a InvalidCharacterError will be thrown if one of the tokens contains
+	 * ASCII whitespace.
+	 * @param  string $aToken A token to check against the token list
+	 * @return bool           Returns true if the token is present, and false
+	 *                        otherwise.
+	 */
 	public function contains($aToken) {
 		$this->checkToken($aToken);
 
 		return in_array($aToken, $this->mTokens);
 	}
 
+	/**
+	 * Remove an observer from the list of observers so that it no longer
+	 * receives notifications from this object.
+	 * @param  SplObserver $aObserver The observer object to be removed.
+	 */
 	public function detach(SplObserver $aObserver) {
 		$this->mObservers->detach($aObserver);
 	}
 
+	/**
+	 * Returns the token at the specified index.
+	 * @param  int    $aIndex An integer index.
+	 * @return string         The token at the specified index or null if the
+	 *                        index does not exist.
+	 */
 	public function item($aIndex) {
 		$rv = null;
 
@@ -62,22 +94,40 @@ class DOMTokenList implements ArrayAccess, SplSubject {
 		return $rv;
 	}
 
+	/**
+	 * Used with isset() to check if there is a token at the specified index
+	 * location.
+	 * @param  int  $aOffset An integer index.
+	 * @return bool          Returns true if the index exists in the token list,
+	 *                       otherwise false.
+	 */
 	public function offsetExists($aOffset) {
 		return isset($this->mTokens[$aOffset]);
 	}
 
+	/**
+	 * Returns the token at the specified index.
+	 * @param  int 	  $aOffset An integer index.
+	 * @return string          The token at the specified index or null if the
+	 *                         index does not exist.
+	 */
 	public function offsetGet($aOffset) {
 		return isset($this->mTokens) ? $this->mTokens[$aOffset] : null;
 	}
 
-	public function offsetSet($aOffset, $aValue) {
+	/**
+	 * Tokens cannont be created or their values modified using array notation.
+	 */
+	public function offsetSet($aOffset, $aValue) {}
+	public function offsetUnset($aOffset) {}
 
-	}
-
-	public function offsetUnset($aOffset) {
-
-	}
-
+	/**
+	 * Removes all arguments if they are present in the token list.
+	 * A SyntaxError will be thrown if one of the tokens is an empty string
+	 * and a InvalidCharacterError will be thrown if one of the tokens contains
+	 * ASCII whitespace.
+	 * @param  string $aTokens... One or more tokens to be removed.
+	 */
 	public function remove($aTokens) {
 		if (!func_num_args()) {
 			return;
@@ -95,6 +145,19 @@ class DOMTokenList implements ArrayAccess, SplSubject {
 		$this->notify();
 	}
 
+	/**
+	 * Adds or removes a token from the list based on whether or not it is
+	 * presently in the list.  A SyntaxError will be thrown if one of the tokens
+	 * is an empty string and a InvalidCharacterError will be thrown if one of
+	 * the tokens contains ASCII whitespace.
+	 * @param  string $aToken The token to be toggled.
+	 * @param  bool   $aForce If the token is present and $aForce is null or
+	 *                        false, the token is removed from the list.  If the
+	 *                        token is not present and $aForce is null or true,
+	 *                        the token is added to the list.
+	 * @return bool           Returns true if the token is present in the list,
+	 *                        otherwise false.
+	 */
 	public function toggle($aToken, $aForce = null) {
 		$contains = $this->contains($aToken);
 
@@ -119,10 +182,20 @@ class DOMTokenList implements ArrayAccess, SplSubject {
 		return $rv;
 	}
 
+	/**
+	 * Returns the list of tokens as a string with each token separated by a
+	 * space.
+	 * @return string Stringified token list.
+	 */
 	public function __toString() {
 		return implode(' ', $this->mTokens);
 	}
 
+	/**
+	 * Takes a single token and checks to make sure it is not an empty string
+	 * and does not contain any ASCII whitespace characters.
+	 * @param  string $aToken A token to validate.
+	 */
 	private function checkToken($aToken) {
 		if (empty($aToken)) {
 			throw new SyntaxError;
@@ -133,6 +206,9 @@ class DOMTokenList implements ArrayAccess, SplSubject {
 		}
 	}
 
+	/**
+	 * Notifies all observers when a change occurs.
+	 */
 	public function notify() {
 		foreach($this->mObservers as $observer) {
 			$observer->update($this);
