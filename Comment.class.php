4476bb1c11f123d66170d7b98f71bc4c7e50a1d9
Trevor Rowbotham <smokey101stair@gmail.com>
2015-05-08 17:44:59 -0400

    Add support for Comment nodes

---
+++ Comment.class.php
@@ -0,0 +1,20 @@
+<?php
+// https://developer.mozilla.org/en-US/docs/Web/API/Comment
+// https://dom.spec.whatwg.org/#comment
+
+require_once 'CharacterData.class.php';
+
+class Comment extends CharacterData {
+	public function __construct($aData = '') {
+		parent::__construct();
+
+		$this->mData = $aData;
+		$this->mLength = strlen($aData);
+		$this->mNodeName = '#comment';
+		$this->mNodeType = Node::COMMENT_NODE;
+	}
+
+	public function toHTML() {
+		return '<!-- ' . $this->mData . ' -->';
+	}
+}
