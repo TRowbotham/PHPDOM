df3f6d5e0ed6cbf45e05c1f3bce37b9431b71094
Trevor Rowbotham <smokey101stair@gmail.com>
2015-05-16 19:49:04 -0400

    Initial commit

--- URL.class.php
+++ URL.class.php
@@ -5,15 +5,69 @@
 require_once 'URLUtils.class.php';
 
 class URL extends URLUtils {
-	public function __construct($aUrlString, $aBaseUrl = null) {
+    protected $mBase;
+    protected $mFlags;
+    protected $mFragment;
+    protected $mHost;
+    protected $mObject;
+    protected $mPassword;
+    protected $mPath;
+    protected $mPort;
+    protected $mQuery;
+    protected $mScheme;
+    protected $mSchemeData;
+    protected $mUsername;
+
+	public function __construct() {
 		parent::__construct();
+
+        $this->mBase = null;
+        $this->mFlags = 0;
+        $this->mFragment = null;
+        $this->mHost = null;
+        $this->mObject = null; // Something about supporting Blobs
+        $this->mPassword = null;
+        $this->mPath = new SplDoublyLinkedList();
+        $this->mPort = '';
+        $this->mQuery = null;
+        $this->mScheme = '';
+        $this->mSchemeData = '';
+        $this->mUsername = '';
+
+        $args = func_get_args();
+        $numArgs = func_num_args();
+        $parsedBase = null;
+
+        if ($numArgs > 1) {
+            if ($args[1]) {
+                $parsedBase = URLParser::basicURLParser($args[0]);
+
+                if ($parsedBase === false) {
+                    throw new TypeError('Error parsing URL.');
+                }
+
+                $parsedBase->mUrl = $parsedBase;
+            }
+        }
+
+        if ($numArgs > 0) {
+            $parsedURL = URLParser::basicURLParser($args[0], $parsedBase, null, $this);
+
+            if ($parsedURL === false) {
+                throw new TypeError('Error parsing URL.');
+            }
+
+            $parsedURL->mUrl = $parsedURL;
+        }
 	}
 
-	public static function createObjectURL($aBlob) {
-		// TODO
-	}
+    public static function domainToASCII($aDomain) {
+        $result = iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', $aDomain);
 
-	public static function revokeObjectURL($aObjectURL) {
-		// TODO
-	}
+        return $result;
+    }
+
+    public static function domainToUnicode($aDomain) {
+        return $aDomain;
+    }
 }
