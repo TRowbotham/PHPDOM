ec9297110fa0b6e7fcda327b4f9043b711382a2f
Trevor Rowbotham <smokey101stair@gmail.com>
2015-05-26 04:16:21 -0400

    Implement getElementById() method

--- NonElementParentNode.class.php
+++ NonElementParentNode.class.php
@@ -1,6 +1,20 @@
 <?php
 trait NonElementParentNode {
+    /**
+     * Returns the first element in tree order whose id attribute is equal to $aElementId or
+     * null if no element is found.
+     * @param  string       $aElementId The id of the element you are trying to find.
+     * @return Element|null
+     */
     public function getElementById($aElementId) {
-        // TODO
+        if (!is_string($aElementId)) {
+            return null;
+        }
+
+        $tw = new TreeWalker($this, NodeFilter::SHOW_ELEMENT, function($aNode) use ($aElementId) {
+            return $aNode->id == $aElementId ? NodeFilter::FILTER_ACCEPT : NodeFilter::FILTER_SKIP;
+        });
+
+        return $tw->nextNode();
     }
 }
