2e921c534d2576c50f6f394beddf6be90eafff6f
Trevor Rowbotham <smokey101stair@gmail.com>
2015-06-02 21:56:59 -0400

    Implement li element interface

---
+++ HTMLElement/HTMLLIElement.class.php
@@ -0,0 +1,41 @@
+<?php
+// https://html.spec.whatwg.org/multipage/semantics.html#the-li-element
+
+require_once 'HTMLElement.class.php';
+
+class HTMLLIElement extends HTMLElement {
+    private $mValue;
+
+    public function __construct($aTagName) {
+        parent::__construct($aTagName);
+
+        $this->mValue = '';
+    }
+
+    public function __get($aName) {
+        switch ($aName) {
+            case 'value':
+                return $this->mValue;
+
+            default:
+                return parent::__get($aName);
+        }
+    }
+
+    public function __set($aName, $aValue) {
+        switch ($aName) {
+            case 'value':
+                if (!is_string($aValue)) {
+                    break;
+                }
+
+                $this->mValue = $aValue;
+                $this->_updateAttributeOnPropertyChange($aName, $aValue);
+
+                break;
+
+            default:
+                parent::__set($aName, $aValue);
+        }
+    }
+}
