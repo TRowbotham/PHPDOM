f356873825ec5cd5dcf4397227016378d178f68c
Trevor Rowbotham <smokey101stair@gmail.com>
2015-09-28 17:41:16 -0400

    Implement the replaceData method

--- CharacterData.class.php
+++ CharacterData.class.php
@@ -72,8 +72,62 @@ abstract class CharacterData extends Node {
         // TODO
     }
 
+    /**
+     * Replaces a portion of the string with the provided data begining at the given
+     * offset and lasting until the given count.
+     *
+     * @link   https://dom.spec.whatwg.org/#concept-CD-replace
+     *
+     * @param  int      $aOffset The position within the string where the replacement should begin.
+     *
+     * @param  int      $aCount  The number of characters from the given offset that the replacement should
+     *                           extend to.
+     *
+     * @param  string   $aData   The data to be inserted in to the string.
+     *
+     * @throws IndexSizeError
+     */
     public function replaceData($aOffset, $aCount, $aData) {
-        // TODO
+        $length = $this->length;
+        $count = $aCount;
+
+        if ($aOffset > $length) {
+            throw new IndexSizeError;
+        }
+
+        if ($aOffset + $count > $length) {
+            $count = $length - $aOffset;
+        }
+
+        // TODO: Queue a mutation record for "characterData"
+
+        $this->mData = substr_replace($this->mData, $aData, $aOffset);
+        $deleteOffset = $aOffset + $length;
+        $this->mData = substr($this->mData, 0, $deleteOffset);
+
+        foreach (Range::_getRangeCollection() as $index => $range ) {
+            if ($range->startContainer === $this && $range->startOffset > $aOffset && $range->startOffset <= $aOffset + $count) {
+                $range->setStart($range->startContainer, $aOffset);
+            }
+        }
+
+        foreach (Range::_getRangeCollection() as $index => $range) {
+            if ($range->endContainer === $this && $range->endOffset > $aOffset && $range->endOffset <= $aOffset + $count) {
+                $range->setEnd($range->endContainer, $aOffset);
+            }
+        }
+
+        foreach (Range::_getRangeCollection() as $index => $range) {
+            if ($range->startContainer === $this && $range->startOffset > $aOffset + $count) {
+                $range->setStart($range->startContainer, $range->startOffset + strlen($aData) - $count);
+            }
+        }
+
+        foreach (Range::_getRangeCollection() as $index => $range) {
+            if ($range->endContainer === $this && $range->endOffset > $aOffset + $count) {
+                $range->setEnd($range->endContainer, $range->endOffset + strlen($aData) - $count);
+            }
+        }
     }
 
     public function substringData($aOffset, $aCount) {
