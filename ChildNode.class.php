6c2b3504b2298c2ebe381f9976b502fe3e61fbd6
Trevor Rowbotham <smokey101stair@gmail.com>
2015-09-30 16:33:38 -0400

    Convert tabs to spaces

--- ChildNode.class.php
+++ ChildNode.class.php
@@ -3,53 +3,53 @@
 // https://dom.spec.whatwg.org/#interface-childnode
 
 trait ChildNode {
-	/**
-	 * Inserts any number of Node or DOMString objects after this ChildNode.
-	 * @param  Node|DOMString ...$aNodes A set of Node or DOMString objects to be inserted.
-	 */
-	public function after() {
-		if (!$this->mParentNode || !func_num_args()) {
-			return;
-		}
+    /**
+     * Inserts any number of Node or DOMString objects after this ChildNode.
+     * @param  Node|DOMString ...$aNodes A set of Node or DOMString objects to be inserted.
+     */
+    public function after() {
+        if (!$this->mParentNode || !func_num_args()) {
+            return;
+        }
 
-		$node = $this->mutationMethodMacro(func_get_args());
-		$this->mParentNode->_preinsertNodeBeforeChild($node, $this->mNextSibling);
-	}
+        $node = $this->mutationMethodMacro(func_get_args());
+        $this->mParentNode->_preinsertNodeBeforeChild($node, $this->mNextSibling);
+    }
 
-	/**
-	 * Inserts any number of Node or DOMString objects before this ChildNode.
-	 * @param  Node|DOMString ...$aNodes A set of Node or DOMString objects to be inserted.
-	 */
-	public function before() {
-		if (!$this->mParentNode || !func_num_args()) {
-			return;
-		}
+    /**
+     * Inserts any number of Node or DOMString objects before this ChildNode.
+     * @param  Node|DOMString ...$aNodes A set of Node or DOMString objects to be inserted.
+     */
+    public function before() {
+        if (!$this->mParentNode || !func_num_args()) {
+            return;
+        }
 
-		$node = $this->mutationMethodMacro(func_get_args());
-		$this->mParentNode->_preinsertNodeBeforeChild($node, $this);
-	}
+        $node = $this->mutationMethodMacro(func_get_args());
+        $this->mParentNode->_preinsertNodeBeforeChild($node, $this);
+    }
 
-	/**
-	 * Removes this ChildNode from its ParentNode.
-	 */
-	public function remove() {
-		if (!$this->mParentNode) {
-			return;
-		}
+    /**
+     * Removes this ChildNode from its ParentNode.
+     */
+    public function remove() {
+        if (!$this->mParentNode) {
+            return;
+        }
 
-		$this->mParentNode->_removeChild($this);
-	}
+        $this->mParentNode->_removeChild($this);
+    }
 
-	/**
-	 * Replaces this ChildNode with any number of Node or DOMString objects.
-	 * @param  Node|DOMString ...$aNodes A set of Node or DOMString objects to be inserted in place of this ChildNode.
-	 */
-	public function replaceWith() {
-		if (!$this->parentNode || !func_num_args()) {
-			return;
-		}
+    /**
+     * Replaces this ChildNode with any number of Node or DOMString objects.
+     * @param  Node|DOMString ...$aNodes A set of Node or DOMString objects to be inserted in place of this ChildNode.
+     */
+    public function replaceWith() {
+        if (!$this->parentNode || !func_num_args()) {
+            return;
+        }
 
-		$node = $this->mutationMethodMacro(func_get_args());
-		$this->mParentNode->replaceChild($node, $this);
-	}
+        $node = $this->mutationMethodMacro(func_get_args());
+        $this->mParentNode->replaceChild($node, $this);
+    }
 }
