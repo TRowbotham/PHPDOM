05304f231499231d4ac07ffee03bf25cbf8aaccd
Trevor Rowbotham <smokey101stair@gmail.com>
2015-10-04 19:00:55 -0400

    Convert tabs to spaces

--- ParentNode.class.php
+++ ParentNode.class.php
@@ -3,63 +3,63 @@
 // https://dom.spec.whatwg.org/#interface-parentnode
 
 trait ParentNode {
-	private $mChildren = array();
-	private $mChildElementCount = 0;
-	private $mInvalidateChildren = true;
+    private $mChildren = array();
+    private $mChildElementCount = 0;
+    private $mInvalidateChildren = true;
 
-	/**
-	 * Inserts nodes after the last child of this node, while replacing strings in nodes
-	 * with equvilant Text nodes.
-	 * @param Node|DOMString ...$aNodes One or more Nodes or strings to be appended to this Node.
-	 */
-	public function append() {
-		$node = $this->mutationMethodMacro(func_get_args());
-		$this->_preinsertNodeBeforeChild($node, null);
-	}
+    /**
+     * Inserts nodes after the last child of this node, while replacing strings in nodes
+     * with equvilant Text nodes.
+     * @param Node|DOMString ...$aNodes One or more Nodes or strings to be appended to this Node.
+     */
+    public function append() {
+        $node = $this->mutationMethodMacro(func_get_args());
+        $this->_preinsertNodeBeforeChild($node, null);
+    }
 
-	/**
-	 * Inserts nodes before the first child of this node, while replacing strings in nodes with
-	 * equivalent Text nodes.
-	 * @param Node|DOMString ...$aNodes One or more Nodes or strings to be prepended to this node;
-	 */
-	public function prepend() {
-		$node = $this->mutationMethodMacro(func_get_args());
-		$this->_preinsertNodeBeforeChild($node, $this->mFirstChild);
-	}
+    /**
+     * Inserts nodes before the first child of this node, while replacing strings in nodes with
+     * equivalent Text nodes.
+     * @param Node|DOMString ...$aNodes One or more Nodes or strings to be prepended to this node;
+     */
+    public function prepend() {
+        $node = $this->mutationMethodMacro(func_get_args());
+        $this->_preinsertNodeBeforeChild($node, $this->mFirstChild);
+    }
 
-	private function filterChildElements($aNode) {
-		return $aNode->nodeType == Node::ELEMENT_NODE;
-	}
+    private function filterChildElements($aNode) {
+        return $aNode->nodeType == Node::ELEMENT_NODE;
+    }
 
-	private function getChildren() {
-		$this->maybeInvalidateChildren();
+    private function getChildren() {
+        $this->maybeInvalidateChildren();
 
-		return $this->mChildElementCount ? $this->mChildren : null;
-	}
+        return $this->mChildElementCount ? $this->mChildren : null;
+    }
 
-	private function getFirstElementChild() {
-		$this->maybeInvalidateChildren();
+    private function getFirstElementChild() {
+        $this->maybeInvalidateChildren();
 
-		return $this->mChildElementCount ? $this->mChildren[0] : null;
-	}
+        return $this->mChildElementCount ? $this->mChildren[0] : null;
+    }
 
-	private function getLastElementChild() {
-		$this->maybeInvalidateChildren();
+    private function getLastElementChild() {
+        $this->maybeInvalidateChildren();
 
-		return $this->mChildElementCount ? $this->mChildren[$this->mChildElementCount - 1] : null;
-	}
+        return $this->mChildElementCount ? $this->mChildren[$this->mChildElementCount - 1] : null;
+    }
 
-	private function getChildElementCount() {
-		$this->maybeInvalidateChildren();
+    private function getChildElementCount() {
+        $this->maybeInvalidateChildren();
 
-		return $this->mChildElementCount;
-	}
+        return $this->mChildElementCount;
+    }
 
-	private function maybeInvalidateChildren() {
-		if ($this->mInvalidateChildren) {
-			$this->mInvalidateChildren = false;
-			$this->mChildren = array_filter($this->mChildNodes, array($this, 'filterChildElements'));
-			$this->mChildElementCount = count($this->mChildren);
-		}
-	}
+    private function maybeInvalidateChildren() {
+        if ($this->mInvalidateChildren) {
+            $this->mInvalidateChildren = false;
+            $this->mChildren = array_filter($this->mChildNodes, array($this, 'filterChildElements'));
+            $this->mChildElementCount = count($this->mChildren);
+        }
+    }
 }
