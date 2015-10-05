307dd77f8bf72d8fc50b167012daa2450432c92c
Trevor Rowbotham <smokey101stair@gmail.com>
2015-10-05 00:00:42 -0400

    Convert tabs to spaces

--- Attr.class.php
+++ Attr.class.php
@@ -3,75 +3,75 @@
 // https://dom.spec.whatwg.org/#attr
 
 class Attr {
-	protected $mLocalName;
-	protected $mName;
-	protected $mNamespaceURI;
-	protected $mOwnerElement;
-	protected $mPrefix;
-	protected $mValue;
+    protected $mLocalName;
+    protected $mName;
+    protected $mNamespaceURI;
+    protected $mOwnerElement;
+    protected $mPrefix;
+    protected $mValue;
 
-	public function __construct(Element $aOwnerElement, $aLocalName, $aValue, $aNamespace = null, $aPrefix = null) {
-		$this->mLocalName = $aLocalName;
-		$this->mName = $aPrefix ? $aPrefix . ':' . $aLocalName : $aLocalName;
-		$this->mNamespaceURI = null;
-		$this->mOwnerElement = $aOwnerElement;
-		$this->mPrefix = $aPrefix;
-		$this->mValue = $aValue;
-	}
+    public function __construct(Element $aOwnerElement, $aLocalName, $aValue, $aNamespace = null, $aPrefix = null) {
+        $this->mLocalName = $aLocalName;
+        $this->mName = $aPrefix ? $aPrefix . ':' . $aLocalName : $aLocalName;
+        $this->mNamespaceURI = null;
+        $this->mOwnerElement = $aOwnerElement;
+        $this->mPrefix = $aPrefix;
+        $this->mValue = $aValue;
+    }
 
-	public function __get($aName) {
-		switch ($aName) {
-			case 'localName':
-				return $this->mLocalName;
+    public function __get($aName) {
+        switch ($aName) {
+            case 'localName':
+                return $this->mLocalName;
 
-			case 'name':
-				return $this->mName;
+            case 'name':
+                return $this->mName;
 
-			case 'namespaceURI':
-				return $this->mNamespaceURI;
+            case 'namespaceURI':
+                return $this->mNamespaceURI;
 
-			case 'ownerElement':
-				return $this->mOwnerElement;
+            case 'ownerElement':
+                return $this->mOwnerElement;
 
-			case 'prefix':
-				return $this->mPrefix;
+            case 'prefix':
+                return $this->mPrefix;
 
-			case 'value':
-				return $this->mValue;
-		}
-	}
+            case 'value':
+                return $this->mValue;
+        }
+    }
 
-	public function __set($aName, $aValue) {
-		switch ($aName) {
-			case 'value':
-				$this->mValue = $aValue;
-		}
-	}
+    public function __set($aName, $aValue) {
+        switch ($aName) {
+            case 'value':
+                $this->mValue = $aValue;
+        }
+    }
 
-	public static function _isBool($aAttributeName) {
-		switch ($aAttributeName) {
-			case 'async':
-			case 'autofocus':
-			case 'autoplay':
-			case 'checked':
-			case 'controls':
-			case 'disabled':
-			case 'default':
-			case 'defer':
-			case 'hidden':
-			case 'ismap':
-			case 'loop':
-			case 'multiple':
-			case 'novalidate':
-			case 'ping':
-			case 'readonly':
-			case 'required':
-			case 'reversed':
-			case 'scoped':
-				return true;
+    public static function _isBool($aAttributeName) {
+        switch ($aAttributeName) {
+            case 'async':
+            case 'autofocus':
+            case 'autoplay':
+            case 'checked':
+            case 'controls':
+            case 'disabled':
+            case 'default':
+            case 'defer':
+            case 'hidden':
+            case 'ismap':
+            case 'loop':
+            case 'multiple':
+            case 'novalidate':
+            case 'ping':
+            case 'readonly':
+            case 'required':
+            case 'reversed':
+            case 'scoped':
+                return true;
 
-			default:
-				return false;
-		}
-	}
+            default:
+                return false;
+        }
+    }
 }
