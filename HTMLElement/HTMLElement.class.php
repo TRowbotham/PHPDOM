7f73e1c7889fef2dd2ff2a80a085743bc4308dd4
Trevor Rowbotham <smokey101stair@gmail.com>
2015-10-05 13:53:31 -0400

    Convert tabs to spaces

--- HTMLElement/HTMLElement.class.php
+++ HTMLElement/HTMLElement.class.php
@@ -3,193 +3,193 @@ require_once 'Element.class.php';
 
 class HTMLElement extends Element {
 
-	protected $mAccessKey;
-	protected $mAccessKeyLabel;
-	protected $mContentEditable;
-	protected $mDir;
-	protected $mDataset;
-	protected $mHidden;
-	protected $mIsContentEditable;
-	protected $mLang;
-	protected $mSpellcheck;
-	protected $mTabIndex;
-	protected $mTitle;
-	protected $mTranslate;
+    protected $mAccessKey;
+    protected $mAccessKeyLabel;
+    protected $mContentEditable;
+    protected $mDir;
+    protected $mDataset;
+    protected $mHidden;
+    protected $mIsContentEditable;
+    protected $mLang;
+    protected $mSpellcheck;
+    protected $mTabIndex;
+    protected $mTitle;
+    protected $mTranslate;
 
-	public function __construct($aTagName) {
-		parent::__construct($aTagName);
+    public function __construct($aTagName) {
+        parent::__construct($aTagName);
 
-		$this->mAccessKey = '';
-		$this->mAccessKeyLabel = '';
-		$this->mContentEditable = false;
-		$this->mDataset;
-		$this->mDir = '';
-		$this->mHidden = false;
-		$this->mIsContentEditable = false;
-		$this->mLang = '';
-		$this->mNodeType = Node::ELEMENT_NODE;
-		$this->mSpellcheck = false;
-		$this->mTabIndex = '';
-		$this->mTitle = '';
-		$this->mTranslate = false;
-	}
+        $this->mAccessKey = '';
+        $this->mAccessKeyLabel = '';
+        $this->mContentEditable = false;
+        $this->mDataset;
+        $this->mDir = '';
+        $this->mHidden = false;
+        $this->mIsContentEditable = false;
+        $this->mLang = '';
+        $this->mNodeType = Node::ELEMENT_NODE;
+        $this->mSpellcheck = false;
+        $this->mTabIndex = '';
+        $this->mTitle = '';
+        $this->mTranslate = false;
+    }
 
-	public function __get($aName) {
-		switch ($aName) {
-			case 'accessKey':
-				return $this->mAccessKey;
-			case 'accessKeyLabel':
-				return $this->mAccessKeyLabel;
-			case 'contentEditable':
-				return $this->mContentEditable;
-			case 'dataset':
-				return $this->mDataset;
-			case 'dir':
-				return $this->mDir;
-			case 'hidden':
-				return $this->mHidden;
-			case 'isContentEditable':
-				return $this->mIsContentEditable;
-			case 'lang':
-				return $this->mLang;
-			case 'spellcheck':
-				return $this->mSpellcheck;
-			case 'tabIndex':
-				return $this->mTabIndex;
-			case 'title':
-				return $this->mTitle;
-			case 'translate':
-				return $this->mTranslate;
-			default:
-				return parent::__get($aName);
-		}
-	}
+    public function __get($aName) {
+        switch ($aName) {
+            case 'accessKey':
+                return $this->mAccessKey;
+            case 'accessKeyLabel':
+                return $this->mAccessKeyLabel;
+            case 'contentEditable':
+                return $this->mContentEditable;
+            case 'dataset':
+                return $this->mDataset;
+            case 'dir':
+                return $this->mDir;
+            case 'hidden':
+                return $this->mHidden;
+            case 'isContentEditable':
+                return $this->mIsContentEditable;
+            case 'lang':
+                return $this->mLang;
+            case 'spellcheck':
+                return $this->mSpellcheck;
+            case 'tabIndex':
+                return $this->mTabIndex;
+            case 'title':
+                return $this->mTitle;
+            case 'translate':
+                return $this->mTranslate;
+            default:
+                return parent::__get($aName);
+        }
+    }
 
-	public function __set($aName, $aValue) {
-		switch ($aName) {
-			case 'accessKey':
-				$this->mAccessKey = $aValue;
-				$this->_updateAttributeOnPropertyChange($aName, $aValue);
+    public function __set($aName, $aValue) {
+        switch ($aName) {
+            case 'accessKey':
+                $this->mAccessKey = $aValue;
+                $this->_updateAttributeOnPropertyChange($aName, $aValue);
 
-				break;
+                break;
 
-			case 'accessKeyLabel':
-				$this->mAccessKeyLabel = $aValue;
-				$this->_updateAttributeOnPropertyChange($aName, $aValue);
+            case 'accessKeyLabel':
+                $this->mAccessKeyLabel = $aValue;
+                $this->_updateAttributeOnPropertyChange($aName, $aValue);
 
-				break;
+                break;
 
-			case 'contentEditable':
-				$this->mContentEditable = $this->mIsContentEditable = $aValue;
-				$this->_updateAttributeOnPropertyChange($aName, $aValue);
+            case 'contentEditable':
+                $this->mContentEditable = $this->mIsContentEditable = $aValue;
+                $this->_updateAttributeOnPropertyChange($aName, $aValue);
 
-				break;
+                break;
 
-			case 'dir':
-				$this->mDir = $aValue;
-				$this->_updateAttributeOnPropertyChange($aName, $aValue);
+            case 'dir':
+                $this->mDir = $aValue;
+                $this->_updateAttributeOnPropertyChange($aName, $aValue);
 
-				break;
+                break;
 
-			case 'hidden':
-				$this->mHidden = $aValue;
-				$this->_updateAttributeOnPropertyChange($aName, $aValue);
+            case 'hidden':
+                $this->mHidden = $aValue;
+                $this->_updateAttributeOnPropertyChange($aName, $aValue);
 
-				break;
+                break;
 
-			case 'lang':
-				$this->mLang = $aValue;
-				$this->_updateAttributeOnPropertyChange($aName, $aValue);
+            case 'lang':
+                $this->mLang = $aValue;
+                $this->_updateAttributeOnPropertyChange($aName, $aValue);
 
-				break;
+                break;
 
-			case 'spellcheck':
-				$this->mSpellcheck = $aValue;
-				$this->_updateAttributeOnPropertyChange($aName, $aValue);
+            case 'spellcheck':
+                $this->mSpellcheck = $aValue;
+                $this->_updateAttributeOnPropertyChange($aName, $aValue);
 
-				break;
+                break;
 
-			case 'tabIndex':
-				$this->mTabIndex = $aValue;
-				$this->_updateAttributeOnPropertyChange($aName, $aValue);
+            case 'tabIndex':
+                $this->mTabIndex = $aValue;
+                $this->_updateAttributeOnPropertyChange($aName, $aValue);
 
-				break;
+                break;
 
-			case 'title':
-				$this->mTitle = $aValue;
-				$this->_updateAttributeOnPropertyChange($aName, $aValue);
+            case 'title':
+                $this->mTitle = $aValue;
+                $this->_updateAttributeOnPropertyChange($aName, $aValue);
 
-				break;
+                break;
 
-			case 'translate':
-				$this->mTranslate = $aValue;
-				$this->_updateAttributeOnPropertyChange($aName, $aValue);
+            case 'translate':
+                $this->mTranslate = $aValue;
+                $this->_updateAttributeOnPropertyChange($aName, $aValue);
 
-				break;
+                break;
 
-			default:
-				parent::__set($aName, $aValue);
-		}
-	}
+            default:
+                parent::__set($aName, $aValue);
+        }
+    }
 
-	public function __toString() {
-		return get_class($this);
-	}
+    public function __toString() {
+        return get_class($this);
+    }
 
-	protected function _onAttributeChange(Event $aEvent) {
-		switch ($aEvent->detail['attr']->name) {
-			case 'accesskey':
-				$this->mAccessKey = $aEvent->detail['action'] == 'set' ? $aEvent->detail['attr']->value : '';
+    protected function _onAttributeChange(Event $aEvent) {
+        switch ($aEvent->detail['attr']->name) {
+            case 'accesskey':
+                $this->mAccessKey = $aEvent->detail['action'] == 'set' ? $aEvent->detail['attr']->value : '';
 
-				break;
+                break;
 
-			case 'accesskeylabel':
-				$this->mAccessKeyLabel = $aEvent->detail['action'] == 'set' ? $aEvent->detail['attr']->value : '';
+            case 'accesskeylabel':
+                $this->mAccessKeyLabel = $aEvent->detail['action'] == 'set' ? $aEvent->detail['attr']->value : '';
 
-				break;
+                break;
 
-			case 'contenteditable':
-				$this->mContentEditable = $this->mIsContentEditable = $aEvent->detail['action'] == 'set' ? true : false;
+            case 'contenteditable':
+                $this->mContentEditable = $this->mIsContentEditable = $aEvent->detail['action'] == 'set' ? true : false;
 
-				break;
+                break;
 
-			case 'dir':
-				$this->mDir = $aEvent->detail['action'] == 'set' ? $aEvent->detail['attr']->value : '';
+            case 'dir':
+                $this->mDir = $aEvent->detail['action'] == 'set' ? $aEvent->detail['attr']->value : '';
 
-				break;
+                break;
 
-			case 'hidden':
-				$this->hidden = $aEvent->detail['action'] == 'set' ? true : false;
+            case 'hidden':
+                $this->hidden = $aEvent->detail['action'] == 'set' ? true : false;
 
-				break;
+                break;
 
-			case 'lang':
-				$this->mLang = $aEvent->detail['action'] == 'set' ? $aEvent->detail['attr']->value : '';
+            case 'lang':
+                $this->mLang = $aEvent->detail['action'] == 'set' ? $aEvent->detail['attr']->value : '';
 
-				break;
+                break;
 
-			case 'spellcheck':
-				$this->spellcheck = $aEvent->detail['action'] == 'set' ? true : false;
+            case 'spellcheck':
+                $this->spellcheck = $aEvent->detail['action'] == 'set' ? true : false;
 
-				break;
+                break;
 
-			case 'tabindex':
-				$this->tabindex = $aEvent->detail['action'] == 'set' ? $aEvent->detail['attr']->value : '';
+            case 'tabindex':
+                $this->tabindex = $aEvent->detail['action'] == 'set' ? $aEvent->detail['attr']->value : '';
 
-				break;
+                break;
 
-			case 'title':
-				$this->mTitle = $aEvent->detail['action'] == 'set' ? $aEvent->detail['attr']->value : '';
+            case 'title':
+                $this->mTitle = $aEvent->detail['action'] == 'set' ? $aEvent->detail['attr']->value : '';
 
-				break;
+                break;
 
-			case 'translate':
-				$this->mTranslate = $aEvent->detail['action'] == 'set' ? true : false;
+            case 'translate':
+                $this->mTranslate = $aEvent->detail['action'] == 'set' ? true : false;
 
-				break;
+                break;
 
-			default:
-				parent::_onAttributeChange($aEvent);
-		}
-	}
+            default:
+                parent::_onAttributeChange($aEvent);
+        }
+    }
 }
