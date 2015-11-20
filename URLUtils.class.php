be2fa1e60e780056740d44ede081a7cb91233d72
Trevor Rowbotham <smokey101stair@gmail.com>
2015-11-20 16:02:14 -0500

    Convert tabs to spaces

--- URLUtils.class.php
+++ URLUtils.class.php
@@ -5,185 +5,185 @@ require_once 'URL.class.php';
 require_once 'URLUtilsReadOnly.class.php';
 
 trait URLUtils {
-	use URLUtilsReadOnly;
+    use URLUtilsReadOnly;
 
-	private function initURLUtils() {
-		$this->initURLUtilsReadOnly();
-	}
+    private function initURLUtils() {
+        $this->initURLUtilsReadOnly();
+    }
 
-	private function URLUtilsSetter($aName, $aValue) {
-		switch ($aName) {
-			case 'hash':
-				if (!$this->mUrl || $this->mUrl->mScheme == 'javascript') {
-					return;
-				}
+    private function URLUtilsSetter($aName, $aValue) {
+        switch ($aName) {
+            case 'hash':
+                if (!$this->mUrl || $this->mUrl->mScheme == 'javascript') {
+                    return;
+                }
 
-				if ($aValue === '') {
-					$this->mUrl->mFragment = null;
-					$this->preupdate();
-					return;
-				}
+                if ($aValue === '') {
+                    $this->mUrl->mFragment = null;
+                    $this->preupdate();
+                    return;
+                }
 
-				$input = $aValue[0] == '#' ? substr($aValue, 1) : $aValue;
-				$this->mUrl->mFragment = '';
-				URLParser::basicURLParser($input, null, null, $this->mUrl, URLParser::STATE_FRAGMENT);
-				$this->preupdate();
+                $input = $aValue[0] == '#' ? substr($aValue, 1) : $aValue;
+                $this->mUrl->mFragment = '';
+                URLParser::basicURLParser($input, null, null, $this->mUrl, URLParser::STATE_FRAGMENT);
+                $this->preupdate();
 
-				break;
+                break;
 
-			case 'host':
-				if ($this->mUrl === null || !($this->mUrl->mFlags & URL::FLAG_RELATIVE)) {
-					return;
-				}
+            case 'host':
+                if ($this->mUrl === null || !($this->mUrl->mFlags & URL::FLAG_RELATIVE)) {
+                    return;
+                }
 
-				URLParser::basicURLParser($aValue, null, null, $this->mUrl, URLParser::STATE_HOST);
-				$this->preupdate();
+                URLParser::basicURLParser($aValue, null, null, $this->mUrl, URLParser::STATE_HOST);
+                $this->preupdate();
 
-				break;
+                break;
 
-			case 'hostname':
-				if ($this->mUrl === null || !($this->mUrl->mFlags & URL::FLAG_RELATIVE)) {
-					return;
-				}
+            case 'hostname':
+                if ($this->mUrl === null || !($this->mUrl->mFlags & URL::FLAG_RELATIVE)) {
+                    return;
+                }
 
-				URLParser::basicURLParser($aValue, null, null, $this->mUrl, URLParser::STATE_HOSTNAME);
-				$this->preupdate();
+                URLParser::basicURLParser($aValue, null, null, $this->mUrl, URLParser::STATE_HOSTNAME);
+                $this->preupdate();
 
-				break;
+                break;
 
-			case 'href':
-				$input = $aValue;
+            case 'href':
+                $input = $aValue;
 
-				if ($this instanceof URL) {
-					$parsedUrl = URLParser::basicURLParser($input, $this->getBaseURL());
+                if ($this instanceof URL) {
+                    $parsedUrl = URLParser::basicURLParser($input, $this->getBaseURL());
 
-					if ($parsedUrl === false) {
-						throw new TypeError('Failed to parse URL.');
-					}
+                    if ($parsedUrl === false) {
+                        throw new TypeError('Failed to parse URL.');
+                    }
 
-					$this->setURLInput('', $parsedUrl);
-				} else {
-					$this->setURLInput($input);
-					$this->preupdate($input);
-				}
+                    $this->setURLInput('', $parsedUrl);
+                } else {
+                    $this->setURLInput($input);
+                    $this->preupdate($input);
+                }
 
-				break;
+                break;
 
-			case 'password':
-				if ($this->mUrl === null || !($this->mUrl->mFlags & URL::FLAG_RELATIVE)) {
-					return;
-				}
+            case 'password':
+                if ($this->mUrl === null || !($this->mUrl->mFlags & URL::FLAG_RELATIVE)) {
+                    return;
+                }
 
-				if ($aValue === '') {
-					$this->mUrl->mPassword = null;
-				} else {
-					$this->mUrl->mPassword = '';
+                if ($aValue === '') {
+                    $this->mUrl->mPassword = null;
+                } else {
+                    $this->mUrl->mPassword = '';
 
-					for ($i = 0; $i < strlen($aValue); $i++) {
-						$this->mUrl->mPassword .= URLParser::utf8PercentEncode($aValue[$i]);
-					}
-				}
+                    for ($i = 0; $i < strlen($aValue); $i++) {
+                        $this->mUrl->mPassword .= URLParser::utf8PercentEncode($aValue[$i]);
+                    }
+                }
 
-				break;
+                break;
 
-			case 'pathname':
-				if ($this->mUrl === null || !($this->mUrl->mFlags & URL::FLAG_RELATIVE)) {
-					return;
-				}
+            case 'pathname':
+                if ($this->mUrl === null || !($this->mUrl->mFlags & URL::FLAG_RELATIVE)) {
+                    return;
+                }
 
-				while (!$this->mUrl->mPath->isEmpty()) {
-					$this->mUrl->mPath->pop();
-				}
+                while (!$this->mUrl->mPath->isEmpty()) {
+                    $this->mUrl->mPath->pop();
+                }
 
-				URLParser::basicURLParser($aValue, null, null, $this->mUrl, URLParser::STATE_RELATIVE_PATH_START);
-				$this->preupdate();
+                URLParser::basicURLParser($aValue, null, null, $this->mUrl, URLParser::STATE_RELATIVE_PATH_START);
+                $this->preupdate();
 
-				break;
+                break;
 
-			case 'port':
-				if ($this->mUrl === null || !($this->mUrl->mFlags & URL::FLAG_RELATIVE) || $this->mUrl->mScheme == 'file') {
-					return;
-				}
+            case 'port':
+                if ($this->mUrl === null || !($this->mUrl->mFlags & URL::FLAG_RELATIVE) || $this->mUrl->mScheme == 'file') {
+                    return;
+                }
 
-				URLParser::basicURLParser($aValue, null, null, $this->mUrl, URLParser::STATE_PORT);
-				$this->preupdate();
+                URLParser::basicURLParser($aValue, null, null, $this->mUrl, URLParser::STATE_PORT);
+                $this->preupdate();
 
-				break;
+                break;
 
-			case 'protocol':
-				if ($this->mUrl === null) {
-					return;
-				}
+            case 'protocol':
+                if ($this->mUrl === null) {
+                    return;
+                }
 
-				URLParser::basicURLParser($aValue . ':', null, null, $this->mUrl, URLParser::STATE_SCHEME_START);
-				$this->preupdate();
+                URLParser::basicURLParser($aValue . ':', null, null, $this->mUrl, URLParser::STATE_SCHEME_START);
+                $this->preupdate();
 
-				break;
+                break;
 
-			case 'search':
-				if ($this->mUrl === null) {
-					return;
-				}
+            case 'search':
+                if ($this->mUrl === null) {
+                    return;
+                }
 
-				if ($aValue === '') {
-					$this->mUrl->mQuery = null;
-					$this->mSearchParams->_mutateList();
-					$this->mSearchParams->notify();
+                if ($aValue === '') {
+                    $this->mUrl->mQuery = null;
+                    $this->mSearchParams->_mutateList();
+                    $this->mSearchParams->notify();
 
-					return;
-				}
+                    return;
+                }
 
-				$input = $aValue[0] == '?' ? substr($aValue, 1) : $aValue;
-				$this->mUrl->mQuery = '';
-				URLParser::basicURLParser($input, null, null, $this->mUrl, URLParser::STATE_QUERY, null);
-				$pairs = URLParser::urlencodedStringParser($input);
-				$this->mSearchParams->_mutateList($pairs);
-				$this->mSearchParams->notify();
+                $input = $aValue[0] == '?' ? substr($aValue, 1) : $aValue;
+                $this->mUrl->mQuery = '';
+                URLParser::basicURLParser($input, null, null, $this->mUrl, URLParser::STATE_QUERY, null);
+                $pairs = URLParser::urlencodedStringParser($input);
+                $this->mSearchParams->_mutateList($pairs);
+                $this->mSearchParams->notify();
 
-				break;
+                break;
 
-			case 'searchParams':
-				$object = $aValue;
-				$this->mSearchParams->detach($this);
-				$object->attach($this);
-				$this->mSearchParams = $object;
-				$this->mUrl->mQuery = $this->mSearchParams->toString();
-				$this->preupdate();
+            case 'searchParams':
+                $object = $aValue;
+                $this->mSearchParams->detach($this);
+                $object->attach($this);
+                $this->mSearchParams = $object;
+                $this->mUrl->mQuery = $this->mSearchParams->toString();
+                $this->preupdate();
 
-				break;
+                break;
 
-			case 'username':
-				if ($this->mUrl === null || !($this->mUrl->mFlags & URL::FLAG_RELATIVE)) {
-					return;
-				}
+            case 'username':
+                if ($this->mUrl === null || !($this->mUrl->mFlags & URL::FLAG_RELATIVE)) {
+                    return;
+                }
 
-				$this->mUsername = '';
+                $this->mUsername = '';
 
-				for ($i = 0; $i < strlen($aValue); $i++) {
-					$this->mUrl->mUsername .= URLParser::utf8PercentEncode($aValue[$i], URLParser::ENCODE_SET_USERNAME);
-				}
+                for ($i = 0; $i < strlen($aValue); $i++) {
+                    $this->mUrl->mUsername .= URLParser::utf8PercentEncode($aValue[$i], URLParser::ENCODE_SET_USERNAME);
+                }
 
-				$this->preupdate();
+                $this->preupdate();
 
-				break;
+                break;
 
-			default:
-				if (property_exists($this, $aName)) {
-					$this->$aName = $aValue;
-				}
-		}
-	}
+            default:
+                if (property_exists($this, $aName)) {
+                    $this->$aName = $aValue;
+                }
+        }
+    }
 
-	private function preupdate($aValue = null) {
-		$value = $aValue;
+    private function preupdate($aValue = null) {
+        $value = $aValue;
 
-		if ($value === null) {
-			$value = URLParser::serializeURL($this->mUrl);
-		}
+        if ($value === null) {
+            $value = URLParser::serializeURL($this->mUrl);
+        }
 
-		$this->updateURL($value);
-	}
+        $this->updateURL($value);
+    }
 
-	abstract protected function updateURL($aValue);
-	abstract protected function getBaseURL();
+    abstract protected function updateURL($aValue);
+    abstract protected function getBaseURL();
 }
