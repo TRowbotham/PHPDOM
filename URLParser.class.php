5a661d8b98cd211a23f2c8cb72e02656db0489e9
Trevor Rowbotham <smokey101stair@gmail.com>
2015-11-20 19:20:12 -0500

    Escape regex delimiter

--- URLParser.class.php
+++ URLParser.class.php
@@ -1072,7 +1072,7 @@ class URLParser {
         }
 
         if (!$inCodeSet && $aEncodeSet <= self::ENCODE_SET_USERINFO) {
-            $inCodeSet = $inCodeSet || preg_match('/[/:;=@[\\\]^|]/', $aCodePoint);
+            $inCodeSet = $inCodeSet || preg_match('/[\/:;=@[\\\]^|]/', $aCodePoint);
         }
 
         if (!$inCodeSet) {
