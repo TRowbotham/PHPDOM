d1abc712d5fb65082057920f19a359e108bdc99d
Trevor Rowbotham <smokey101stair@gmail.com>
2015-11-22 02:11:43 -0500

    Inline URL attributes
    URLUtils and URLReadOnlyUtils were removed from the spec and their
    member properties were inlined in to URL

--- URL.class.php
+++ URL.class.php
@@ -1,72 +1,241 @@
 <?php
-// https://developer.mozilla.org/en-US/docs/Web/API/URL
-// https://url.spec.whatwg.org/#api
+namespace phpjs\url;
+
+use phpjs\url;
 
 require_once 'Exceptions.class.php';
-require_once 'URLUtils.class.php';
+require_once 'URLParser.class.php';
+require_once 'URLSearchParams.class.php';
 
-class URL implements SplObserver {
-    use URLUtils;
+/**
+ * Represents a URL that can be manipulated.
+ *
+ * @link https://url.spec.whatwg.org/#api
+ * @link https://developer.mozilla.org/en-US/docs/Web/API/URL
+ */
+class URL {
+    private $mSearchParams;
+    private $mUrl;
 
-    const FLAG_ARRAY = 1;
-    const FLAG_AT = 2;
-    const FLAG_NON_RELATIVE = 4;
-
-    public $mFlags;
-    public $mFragment;
-    public $mHost;
-    public $mPassword;
-    public $mPath;
-    public $mPort;
-    public $mQuery;
-    public $mScheme;
-    public $mUsername;
-
-	public function __construct() {
-        $this->initURLUtils();
-        $this->mFlags = 0;
-        $this->mFragment = null;
-        $this->mHost = null;
-        $this->mPassword = null;
-        $this->mPath = new SplDoublyLinkedList();
-        $this->mPort = null;
-        $this->mQuery = null;
-        $this->mScheme = '';
-        $this->mUsername = '';
-
-        $args = func_get_args();
-        $numArgs = func_num_args();
+	public function __construct($aUrl, $aBase = null) {
+        $this->mSearchParams = null;
+        $this->mUrl = null;
         $parsedBase = null;
 
-        if ($numArgs > 1) {
-            if ($args[1]) {
-                $parsedBase = URLParser::basicURLParser($args[1]);
+        if ($aBase) {
+            $parsedBase = URLParser2::basicURLParser($aBase);
 
-                if ($parsedBase === false) {
-                    throw new TypeError($args[1] . ' is not a valid URL.');
-                }
-
-                $parsedBase->mUrl = $parsedBase;
+            if ($parsedBase === false) {
+                throw new \TypeError($aBase . ' is not a valid URL.');
             }
         }
 
-        if ($numArgs > 0) {
-            $parsedURL = URLParser::basicURLParser($args[0], $parsedBase, null, $this);
+        $parsedURL = URLParser2::basicURLParser($aUrl, $parsedBase);
 
-            if ($parsedURL === false) {
-                throw new TypeError($args[0] . ' is not a valid URL.');
-            }
-
-            $this->setURLInput('', $parsedURL);
+        if ($parsedURL === false) {
+            throw new \TypeError($aUrl . ' is not a valid URL.');
         }
+
+        $this->mUrl = $parsedURL;
+        $this->mSearchParams = new URLSearchParams2($this->mUrl->getQuery());
+        $this->mSearchParams->_setUrl($parsedURL);
 	}
 
     public function __get($aName) {
-        return $this->URLUtilsGetter($aName);
+        switch ($aName) {
+            case 'hash':
+                $fragment = $this->mUrl->getFragment();
+
+                return !$fragment ? '' : '#' . $fragment;
+
+            case 'host':
+                $host = $this->mUrl->getHost();
+                $port = $this->mUrl->getPort();
+
+                if ($host === null) {
+                    return '';
+                }
+
+                if ($port === null) {
+                    return URLParser2::serializeHost($host);
+                }
+
+                return URLParser2::serializeHost($host) . ':' . $port;
+
+            case 'hostname':
+                $host = $this->mUrl->getHost();
+
+                return $host === null ? '' : URLParser2::serializeHost($host);
+
+            case 'href':
+                return URLParser2::serializeURL($this->mUrl);
+
+            case 'origin':
+                // TODO
+
+                return;
+
+            case 'password':
+                $password = $this->mUrl->getPassword();
+
+                return $password === null ? '' : $password;
+
+            case 'pathname':
+                if ($this->mUrl->isFlagSet(URLInternal::FLAG_NON_RELATIVE)) {
+                    return $this->mUrl->getPath()[0];
+                }
+
+                $output = '/';
+
+                foreach ($this->mUrl->getPath() as $key => $path) {
+                    if ($key > 0) {
+                        $output .= '/';
+                    }
+
+                    $output .= $path;
+                }
+
+                return $output;
+
+            case 'port':
+                $port = $this->mUrl->getPort();
+
+                return $port === null ? '' : $port;
+
+            case 'protocol':
+                return $this->mUrl->getScheme() . ':';
+
+            case 'search':
+                $query = $this->mUrl->getQuery();
+
+                return !$query ? '' : '?' . $query;
+
+            case 'searchParams':
+                return $this->mSearchParams;
+
+            case 'username':
+                return $this->mUrl->getUsername();
+
+        }
     }
 
     public function __set($aName, $aValue) {
-        $this->URLUtilsSetter($aName, $aValue);
+        switch ($aName) {
+            case 'hash':
+                if ($this->mUrl->getScheme() == 'javascript') {
+                    // Terminate these steps
+                    return;
+                }
+
+                if ($aValue === '') {
+                    $this->mUrl->setFragment(null);
+
+                    // Terminate these steps
+                    return;
+                }
+
+                $input = $aValue[0] == '#' ? substr($aValue, 1) : $aValue;
+                $this->mUrl->setFragment('');
+                URLParser2::basicURLParser($input, null, null, $this->mUrl, URLParser2::FRAGMENT_STATE);
+
+                break;
+
+            case 'host':
+                if ($this->mUrl->isFlagSet(URLInternal::FLAG_NON_RELATIVE)) {
+                    // Terminate these steps
+                    return;
+                }
+
+                URLParser2::basicURLParser($aValue, null, null, $this->mUrl, URLParser2::HOST_STATE);
+
+                break;
+
+            case 'hostname':
+                if ($this->mUrl->isFlagSet(URLInternal::FLAG_NON_RELATIVE)) {
+                    // Terminate these steps
+                    return;
+                }
+
+                URLParser2::basicURLParser($aValue, null, null, $this->mUrl, URLParser2::HOSTNAME_STATE);
+
+            case 'href':
+                $parsedURL = URLParser2::basicURLParser($aValue);
+
+                if ($parsedURL === false) {
+                    throw new \TypeError($aValue . ' is not a valid URL.');
+                }
+
+                $this->mUrl = $parsedURL;
+                $this->mSearchParams->_setUrl($parsedURL);
+
+                break;
+
+            case 'password':
+                if ($this->mUrl->getHost() === null || $this->mUrl->isFlagSet(URLInternal::FLAG_NON_RELATIVE)) {
+                    // Terminate these steps
+                    return;
+                }
+
+                $this->mUrl->setPasswordSteps($aValue);
+
+                break;
+
+            case 'pathname':
+                if ($this->mUrl->isFlagSet(URLInternal::FLAG_NON_RELATIVE)) {
+                    // Terminate these steps
+                    return;
+                }
+
+                while (!$this->mUrl->getPath()->isEmpty()) {
+                    $this->mUrl->getPath()->pop();
+                }
+
+                URLParser2::basicURLParser($aValue, null, null, $this->mUrl, URLParser2::PATH_START_STATE);
+
+                break;
+
+            case 'port':
+                if ($this->mUrl->getHost() === null || $this->mUrl->isFlagSet(URLInternal::FLAG_NON_RELATIVE) || $this->mUrl->getScheme() == 'file') {
+                    // Terminate these steps
+                    return;
+                }
+
+                URLParser2::basicURLParser($aValue, null, null, $this->mUrl, URLParser2::PORT_STATE);
+
+                break;
+
+            case 'protocol':
+                URLParser2::basicURLParser($aValue . ':', null, null, $this->mUrl, URLParser2::SCHEME_START_STATE);
+
+                break;
+
+            case 'search':
+                $query = $this->mUrl->getQuery();
+
+                if ($aValue === '') {
+                    $this->mUrl->setQuery(null);
+
+                    // TODO: Empty the query object
+                }
+
+                $input = $aValue[0] == '?' ? substr($aValue, 1) : $aValue;
+                $this->mUrl->setQuery('');
+                URLParser2::basicURLParser($input, null, null, $this->mUrl, URLParser2::QUERY_STATE);
+
+                // TODO: Set url’s query object’s list to the result of parsing input.
+
+                break;
+
+            case 'username':
+                if ($this->mUrl->getHost() === null || $this->mUrl->isFlagSet(URLInternal::FLAG_NON_RELATIVE)) {
+                    // Terminate these steps
+                    return;
+                }
+
+                $this->mUrl->setUsernameSteps($aValue);
+
+                break;
+        }
     }
 
     public static function domainToASCII($aDomain) {
@@ -77,13 +246,6 @@ class URL implements SplObserver {
 
     public static function domainToUnicode($aDomain) {
         return $aDomain;
-    }
-
-    public function update(SplSubject $aObject) {
-        if ($aObject instanceof URLSearchParams) {
-            $this->mQuery = $aObject->toString();
-            $this->preupdate();
-        }
     }
 
     /**
@@ -97,17 +259,5 @@ class URL implements SplObserver {
      */
     public function _isSpecial() {
         return array_key_exists($this->mScheme, URLParser::$specialSchemes);
-    }
-
-    private function getBaseURL() {
-        $ssl = isset($_SERVER['HTTPS']) && $_SERVER["HTTPS"] == 'on';
-        $port = in_array($_SERVER['SERVER_PORT'], array(80, 443)) ? '' : ':' . $_SERVER['SERVER_PORT'];
-        $url = ($ssl ? 'https' : 'http') . '://' . $_SERVER['SERVER_NAME'] . $port . $_SERVER['REQUEST_URI'];
-
-        return URLParser::basicURLParser($url);
-    }
-
-    private function updateURL($aValue) {
-
     }
 }
