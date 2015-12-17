82cf4e17e1fafc87fa3fea5003c31cfb76e381d7
Trevor Rowbotham <smokey101stair@gmail.com>
2015-12-16 23:12:22 -0500

    Convert tabs to spaces

--- Comment.class.php
+++ Comment.class.php
@@ -5,15 +5,15 @@
 require_once 'CharacterData.class.php';
 
 class Comment extends CharacterData {
-	public function __construct($aData = '') {
-		parent::__construct();
+    public function __construct($aData = '') {
+        parent::__construct();
 
-		$this->mData = $aData;
-		$this->mNodeName = '#comment';
-		$this->mNodeType = Node::COMMENT_NODE;
-	}
+        $this->mData = $aData;
+        $this->mNodeName = '#comment';
+        $this->mNodeType = Node::COMMENT_NODE;
+    }
 
-	public function toHTML() {
-		return '<!-- ' . $this->mData . ' -->';
-	}
+    public function toHTML() {
+        return '<!-- ' . $this->mData . ' -->';
+    }
 }
--- Text.class.php
+++ Text.class.php
@@ -8,99 +8,103 @@ require_once 'CharacterData.class.php';
  * @link https://developer.mozilla.org/en-US/docs/Web/API/Text
  *
  * @property-read string $wholeText Returns the concatenated string data of all contingious Text nodes relative
- * 									to this Node in tree order.
+ *                                  to this Node in tree order.
  */
 class Text extends CharacterData {
-	public function __construct($aData = '') {
-		parent::__construct();
+    public function __construct($aData = '') {
+        parent::__construct();
 
-		$this->mData = $aData;
-		$this->mNodeName = '#text';
-		$this->mNodeType = Node::TEXT_NODE;
-	}
+        $this->mData = $aData;
+        $this->mNodeName = '#text';
+        $this->mNodeType = Node::TEXT_NODE;
+    }
 
-	public function __get($aName) {
-		switch ($aName) {
-			case 'wholeText':
-				$wholeText = '';
-				$startNode = $this;
+    public function __get($aName) {
+        switch ($aName) {
+            case 'wholeText':
+                $wholeText = '';
+                $startNode = $this;
 
-				while ($startNode) {
-					if (!($startNode->previousSibling instanceof Text)) {
-						break;
-					}
+                while ($startNode) {
+                    if (!($startNode->previousSibling instanceof Text)) {
+                        break;
+                    }
 
-					$startNode = $startNode->previousSibling;
-				}
+                    $startNode = $startNode->previousSibling;
+                }
 
-				while ($startNode instanceof Text) {
-					$wholeText .= $startNode->data;
-					$startNode = $startNode->nextSibling;
-				}
+                while ($startNode instanceof Text) {
+                    $wholeText .= $startNode->data;
+                    $startNode = $startNode->nextSibling;
+                }
 
-				return $wholeText;
+                return $wholeText;
 
-			default:
-				return parent::__get($aName);
-		}
-	}
+            default:
+                return parent::__get($aName);
+        }
+    }
 
-	public function splitText($aOffset) {
-		$length = $this->length;
+    public function __toString() {
+        return $this->mNodeName . ' ' . $this->mData;
+    }
 
-		if ($aOffset > $length) {
-			throw new IndexSizeError;
-		}
+    public function splitText($aOffset) {
+        $length = $this->length;
 
-		$count = $length - $aOffset;
-		$newData = substr($this->mData, $aOffset, $count);
-		$newNode = new Text($newData);
-		$newNode->mOwnerDocument = $this;
+        if ($aOffset > $length) {
+            throw new IndexSizeError;
+        }
 
-		if ($this->mParentNode) {
-			$this->mParentNode->_insertNodeBeforeChild($newNode, $this->mNextSibling);
+        $count = $length - $aOffset;
+        $newData = substr($this->mData, $aOffset, $count);
+        $newNode = new Text($newData);
+        $newNode->mOwnerDocument = $this;
 
-			foreach (Range::_getRangeCollection() as $index => $range) {
-				if ($range->startContainer === $this && $range->startOffset > $aOffset) {
-					$range->setStart($newNode, $range->startOffset - $aOffset);
-				}
-			}
+        if ($this->mParentNode) {
+            $this->mParentNode->_insertNodeBeforeChild($newNode, $this->mNextSibling);
 
-			foreach (Range::_getRangeCollection() as $index => $range) {
-				if ($range->endContainer === $this && $range->endOffset > $aOffset) {
-					$range->setEnd($newNode, $range->endOffset - $aOffset);
-				}
-			}
+            foreach (Range::_getRangeCollection() as $index => $range) {
+                if ($range->startContainer === $this && $range->startOffset > $aOffset) {
+                    $range->setStart($newNode, $range->startOffset - $aOffset);
+                }
+            }
 
-			foreach (Range::_getRangeCollection() as $index => $range) {
-				if ($range->startContainer === $this && $range->startOffset == $this->_getTreeIndex() + 1) {
-					$range->setStart($range->startContainer, $range->startOffset + 1);
-				}
-			}
+            foreach (Range::_getRangeCollection() as $index => $range) {
+                if ($range->endContainer === $this && $range->endOffset > $aOffset) {
+                    $range->setEnd($newNode, $range->endOffset - $aOffset);
+                }
+            }
 
-			foreach (Range::_getRangeCollection() as $index => $range) {
-				if ($range->endContainer === $this && $range->endOffset == $this->_getTreeIndex() + 1) {
-					$range->setEnd($range->endContainer, $range->endOffset + 1);
-				}
-			}
-		}
+            foreach (Range::_getRangeCollection() as $index => $range) {
+                if ($range->startContainer === $this && $range->startOffset == $this->_getTreeIndex() + 1) {
+                    $range->setStart($range->startContainer, $range->startOffset + 1);
+                }
+            }
 
-		$this->replaceData($aOffset, $count, $this->mData);
+            foreach (Range::_getRangeCollection() as $index => $range) {
+                if ($range->endContainer === $this && $range->endOffset == $this->_getTreeIndex() + 1) {
+                    $range->setEnd($range->endContainer, $range->endOffset + 1);
+                }
+            }
+        }
 
-		if (!$this->mParentNode) {
-			foreach (Range::_getRangeCollection() as $index => $range) {
-				if ($range->startContainer === $this && $range->startOffset > $aOffset) {
-					$range->setStart($range->startContainer, $aOffset);
-				}
-			}
+        $this->replaceData($aOffset, $count, $this->mData);
 
-			foreach (Range::_getRangeCollection() as $index => $range) {
-				if ($range->endContainer === $this && $range->endOffset > $aOffset) {
-					$range->setEnd($range->endContainer, $aOffset);
-				}
-			}
-		}
+        if (!$this->mParentNode) {
+            foreach (Range::_getRangeCollection() as $index => $range) {
+                if ($range->startContainer === $this && $range->startOffset > $aOffset) {
+                    $range->setStart($range->startContainer, $aOffset);
+                }
+            }
 
-		return $newNode;
-	}
+            foreach (Range::_getRangeCollection() as $index => $range) {
+                if ($range->endContainer === $this && $range->endOffset > $aOffset) {
+                    $range->setEnd($range->endContainer, $aOffset);
+                }
+            }
+        }
+
+        return $newNode;
+    }
 }
