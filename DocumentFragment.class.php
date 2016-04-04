675b3acc488cee40f0e97df09cc1f845cfec95e0
Trevor Rowbotham <smokey101stair@gmail.com>
2016-04-03 23:37:38 -0400

    Allow a DocumentFragment to be "hosted" by another element
    This is necessary to support the HTML template element and shadow roots
    as their contents are contained in DocumentFragments that are then
    hosted by other elements.

--- DocumentFragment.class.php
+++ DocumentFragment.class.php
@@ -1,6 +1,8 @@
 <?php
 namespace phpjs;
 
+use phpjs\elements\Element;
+
 /**
  * @see https://dom.spec.whatwg.org/#interface-documentfragment
  * @see https://developer.mozilla.org/en-US/docs/Web/API/DocumentFragment
@@ -10,10 +12,13 @@ class DocumentFragment extends Node
     use NonElementParentNode;
     use ParentNode;
 
+    protected $mHost;
+
     public function __construct()
     {
         parent::__construct();
 
+        $this->mHost = null;
         $this->mNodeName = '#document-fragment';
         $this->mNodeType = Node::DOCUMENT_FRAGMENT_NODE;
     }
@@ -36,5 +41,20 @@ class DocumentFragment extends Node
             default:
                 return parent::__get($aName);
         }
+    }
+
+    /**
+     * Sets a DocumentFragment's host element, if it has one.
+     *
+     * @internal
+     *
+     * @see https://dom.spec.whatwg.org/#concept-documentfragment-host
+     *
+     * @param Element|null $aHost The element that is hosting the
+     *     DocumentFragment such as a template element or shadow root.
+     */
+    public function setHost(Element $aHost = null)
+    {
+        $this->mHost = $aHost;
     }
 }
