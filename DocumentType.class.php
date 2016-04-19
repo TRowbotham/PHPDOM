d2fd2184d841b3786e64dc3f9cbb5e5399fd0c05
Trevor Rowbotham <smokey101stair@gmail.com>
2016-04-19 16:25:21 -0400

    Convert tabs to spaces

--- DocumentType.class.php
+++ DocumentType.class.php
@@ -7,49 +7,48 @@ namespace phpjs;
  */
 class DocumentType extends Node
 {
-	use ChildNode;
+    use ChildNode;
 
-	private $mName;
-	private $mPublicId;
-	private $mSystemId;
+    private $mName;
+    private $mPublicId;
+    private $mSystemId;
 
-	public function __construct($aName, $aPublicId = '', $aSystemId = '')
+    public function __construct($aName, $aPublicId = '', $aSystemId = '')
     {
-		parent::__construct();
+        parent::__construct();
 
-		$this->mName = $aName;
-		$this->mNodeName = $aName;
-		$this->mNodeType = self::DOCUMENT_TYPE_NODE;
-		$this->mPublicId = $aPublicId;
-		$this->mSystemId = $aSystemId;
-	}
+        $this->mName = $aName;
+        $this->mNodeType = self::DOCUMENT_TYPE_NODE;
+        $this->mPublicId = $aPublicId;
+        $this->mSystemId = $aSystemId;
+    }
 
-	public function __get($aName)
+    public function __get($aName)
     {
-		switch ($aName) {
-			case 'name':
-				return $this->mName;
-			case 'publicId':
-				return $this->mPublicId;
-			case 'systemId':
-				return $this->mSystemId;
-			default:
-				return parent::__get($aName);
-		}
-	}
+        switch ($aName) {
+            case 'name':
+                return $this->mName;
+            case 'publicId':
+                return $this->mPublicId;
+            case 'systemId':
+                return $this->mSystemId;
+            default:
+                return parent::__get($aName);
+        }
+    }
 
-	public function toHTML()
+    public function toHTML()
     {
-		$html = '<!DOCTYPE';
-		$html .= ($this->mName ? ' ' . $this->mName  : '');
-		$html .= ($this->mPublicId ? ' ' . $this->mPublicId : '');
-		$html .= ($this->mSystemId ? ' ' . $this->mSystemId : '');
-		$html .= '>';
+        $html = '<!DOCTYPE';
+        $html .= ($this->mName ? ' ' . $this->mName  : '');
+        $html .= ($this->mPublicId ? ' ' . $this->mPublicId : '');
+        $html .= ($this->mSystemId ? ' ' . $this->mSystemId : '');
+        $html .= '>';
 
-		return $html;
-	}
+        return $html;
+    }
 
-	/**
+    /**
      * Returns the Node's length.
      *
      * @internal
