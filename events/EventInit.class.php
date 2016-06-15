8c30c0bccf594898d1fa169fb67787d2d25ccb8a
Trevor Rowbotham <smokey101stair@gmail.com>
2016-06-14 23:11:51 -0400

    Add composed member to the EventInit dictionary

--- events/EventInit.class.php
+++ events/EventInit.class.php
@@ -5,10 +5,12 @@ class EventInit
 {
     public $bubbles;
     public $cancelable;
+    public $composed;
 
     public function __construct()
     {
         $this->bubbles = false;
         $this->cancelable = false;
+        $this->composed = false;
     }
 }
