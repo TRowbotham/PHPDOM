3341c3a50c544073b3dcd16cbaffe55f6defc653
Trevor Rowbotham <smokey101stair@gmail.com>
2016-11-24 23:43:08 -0500

    Fix missing semi-colon

--- src/exceptions/HierarchyRequestError.class.php
+++ src/exceptions/HierarchyRequestError.class.php
@@ -9,7 +9,7 @@ class HierarchyRequestError extends DOMException
     public function __construct($message = '', $previous = null)
     {
         if ($message === '') {
-            $message = 'The operation would yield an incorrect node tree.'
+            $message = 'The operation would yield an incorrect node tree.';
         }
 
         parent::__construct($message, 3, $previous);
