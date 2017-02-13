969632b4be810bafab704dd6321ad9030bebb069
Trevor Rowbotham <smokey101stair@gmail.com>
2017-02-12 19:24:17 -0500

    Add the XMLDocument class

---
+++ src/XMLDocument.php
@@ -0,0 +1,15 @@
+<?php
+namespace phpjs;
+
+/**
+ * Represents an XML document.
+ *
+ * @see https://dom.spec.whatwg.org/#xmldocument
+ */
+class XMLDocument extends Document
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
