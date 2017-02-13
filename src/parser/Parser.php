4196b972c5459ae8f249a229966f11211d3b7098
Trevor Rowbotham <smokey101stair@gmail.com>
2017-02-12 19:56:38 -0500

    Add an abstract parser class

---
+++ src/parser/Parser.php
@@ -0,0 +1,20 @@
+<?php
+namespace phpjs\parser;
+
+use phpjs\support\CodePointStream;
+
+abstract class Parser
+{
+    const VOID_TAGS = '/^(area|base|basefont|bgsound|br|col|embed|frame|hr|' .
+        'img|input|keygen|link|menuitem|meta|param|source|track|wbr)$/';
+
+    protected $inputStream;
+
+    public function __construct()
+    {
+        $this->inputStream = new CodePointStream();
+    }
+
+    abstract public function abort();
+    abstract public function preprocessInputStream($aInput);
+}
