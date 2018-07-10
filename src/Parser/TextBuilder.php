cd930bc1da1315598a56953bc2c92827fd0ce5da
Trevor Rowbotham <smokey101stair@gmail.com>
2018-07-10 16:35:29 -0400

    Add documentation and type hints for TextBuilder

--- src/Parser/TextBuilder.php
+++ src/Parser/TextBuilder.php
@@ -1,31 +1,62 @@
 <?php
 namespace Rowbot\DOM\Parser;
 
-use Rowbot\DOM\Node;
+use Rowbot\DOM\CharacterData;
 
 class TextBuilder
 {
+    /**
+     * @var \Rowbot\DOM\CharacterData|null
+     */
     private $node;
+
+    /**
+     * @var string
+     */
     private $data;
 
+    /**
+     * Constructor.
+     *
+     * @return void
+     */
     public function __construct()
     {
         $this->data = '';
     }
 
-    public function append(string $text)
+    /**
+     * Appends string to the text builder.
+     *
+     * @param string $text
+     *
+     * @return void
+     */
+    public function append(string $text): void
     {
         $this->data .= $text;
     }
 
-    public function setNode(Node $node)
+    /**
+     * Sets the text node that the text builder is operating on.
+     *
+     * @param \Rowbot\DOM\CharacterData $node
+     *
+     * @return void
+     */
+    public function setNode(CharacterData $node): void
     {
         $this->node = $node;
     }
 
-    public function flushText()
+    /**
+     * Adds the accumulated string data to the node.
+     *
+     * @return void
+     */
+    public function flushText(): void
     {
-        if (!$this->node) {
+        if ($this->node === null) {
             return;
         }
 
