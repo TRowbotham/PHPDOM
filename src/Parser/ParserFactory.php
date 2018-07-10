c736a03c0716e83c86e78df0bbd21d4603f3e7f2
Trevor Rowbotham <smokey101stair@gmail.com>
2018-07-10 17:13:16 -0400

    Update documentation

--- src/Parser/ParserFactory.php
+++ src/Parser/ParserFactory.php
@@ -3,20 +3,30 @@ namespace Rowbot\DOM\Parser;
 
 use Rowbot\DOM\HTMLDocument;
 use Rowbot\DOM\Element\Element;
-use Rowbot\DOM\Exception\DOMException;
-use Rowbot\DOM\Exception\InvalidStateError;
-use Rowbot\DOM\Namespaces;
-use Rowbot\DOM\Node;
 use Rowbot\DOM\Parser\HTML\HTMLParser;
 use Rowbot\DOM\Parser\XML\XMLParser;
 
-abstract class ParserFactory
+final class ParserFactory
 {
-    public static function parseHTMLDocument($aMarkup)
+    /**
+     * Constructor.
+     *
+     * @return void
+     */
+    private function __construct()
+    {
+    }
+
+    /**
+     * @param string $markup
+     *
+     * @return \Rowbot\DOM\HTMLDocument
+     */
+    public static function parseHTMLDocument($markup)
     {
         $doc = new HTMLDocument();
         $parser = new HTMLParser($doc);
-        $parser->preprocessInputStream($aMarkup);
+        $parser->preprocessInputStream($markup);
         $parser->run();
 
         return $doc;
@@ -25,25 +35,24 @@ abstract class ParserFactory
     /**
      * @see https://w3c.github.io/DOM-Parsing/#dfn-fragment-parsing-algorithm
      *
-     * @param string $aMarkup [description]
+     * @param string                      $markup
+     * @param \Rowbot\DOM\Element\Element $contextElement
      *
-     * @param Element $aContextElement [description]
-     *
-     * @return DocumentFragment
+     * @return \Rowbot\DOM\DocumentFragment
      */
-    public static function parseFragment($aMarkup, Element $aContextElement)
+    public static function parseFragment($markup, Element $contextElement)
     {
-        $ownerDocument = $aContextElement->getNodeDocument();
+        $ownerDocument = $contextElement->getNodeDocument();
 
         if ($ownerDocument instanceof HTMLDocument) {
             $newChildren = HTMLParser::parseHTMLFragment(
-                $aMarkup,
-                $aContextElement
+                $markup,
+                $contextElement
             );
         } else {
             $newChildren = XMLParser::parseXMLFragment(
-                $aMarkup,
-                $aContextElement
+                $markup,
+                $contextElement
             );
         }
 
