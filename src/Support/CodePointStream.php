563484168ce3db890196782208910c3b226fa36b
Trevor Rowbotham <smokey101stair@gmail.com>
2018-07-10 12:44:45 -0400

    Add documentation and type hints to CodePointStream

--- src/Support/CodePointStream.php
+++ src/Support/CodePointStream.php
@@ -1,51 +1,95 @@
 <?php
 namespace Rowbot\DOM\Support;
 
+use function array_push;
+use function array_unshift;
+use function count;
+use function preg_split;
+
 class CodePointStream
 {
     const SEEK_RELATIVE = 1;
     const SEEK_ABSOLUTE = 2;
 
+    /**
+     * @var string[]
+     */
     private $data;
+
+    /**
+     * @var int
+     */
     private $currentChar;
+
+    /**
+     * @var int
+     */
     private $nextChar;
 
+    /**
+     * Constructor.
+     *
+     * @param string $data
+     *
+     * @return void
+     */
     public function __construct($data = '')
     {
         $this->currentChar = 0;
         $this->nextChar = 0;
 
         if ($data) {
-            $this->data = \preg_split('//u', $data, -1, PREG_SPLIT_NO_EMPTY);
+            $this->data = preg_split('//u', $data, -1, PREG_SPLIT_NO_EMPTY);
             return;
         }
 
         $this->data = [];
     }
 
-    public function append($data)
+    /**
+     * Appends data to the stream.
+     *
+     * @param string $data
+     *
+     * @return void
+     */
+    public function append($data): void
     {
-        $data = \preg_split('//u', $data, -1, PREG_SPLIT_NO_EMPTY);
+        $data = preg_split('//u', $data, -1, PREG_SPLIT_NO_EMPTY);
 
         if (empty($data)) {
             return;
         }
 
-        \array_push($this->data, ...$data);
+        array_push($this->data, ...$data);
     }
 
-    public function prepend($data)
+    /**
+     * Appends data to the stream.
+     *
+     * @param string $data
+     *
+     * @return void
+     */
+    public function prepend($data): void
     {
-        $data = \preg_split('//u', $data, -1, PREG_SPLIT_NO_EMPTY);
+        $data = preg_split('//u', $data, -1, PREG_SPLIT_NO_EMPTY);
 
         if (empty($data)) {
             return;
         }
 
-        \array_unshift($this->data, ...$data);
+        array_unshift($this->data, ...$data);
     }
 
-    public function get($count = 1)
+    /**
+     * Returns $count characters starting from the next character.
+     *
+     * @param int $count
+     *
+     * @return string
+     */
+    public function get(int $count = 1): string
     {
         $this->currentChar = $this->nextChar;
 
@@ -72,7 +116,14 @@ class CodePointStream
         return $str;
     }
 
-    public function peek($count = 1)
+    /**
+     * Gets $count characters starting from the next character without advancing the iterator.
+     *
+     * @param int $count
+     *
+     * @return string
+     */
+    public function peek(int $count = 1): string
     {
         if (!isset($this->data[$this->nextChar])) {
             return '';
@@ -96,28 +147,55 @@ class CodePointStream
         return $str;
     }
 
-    public function isEoS()
+    /**
+     * Returns whether the stream has reached the end.
+     *
+     * @return bool
+     */
+    public function isEoS(): bool
     {
         return !isset($this->data[$this->currentChar]);
     }
 
-    public function length()
+    /**
+     * Returns the number of characters in the stream.
+     *
+     * @return int
+     */
+    public function length(): int
     {
-        return \count($this->data);
+        return count($this->data);
     }
 
-    public function discard()
+    /**
+     * Empties the stream.
+     *
+     * @return void
+     */
+    public function discard(): void
     {
         $this->data = [];
     }
 
-    public function seek($count)
+    /**
+     * Seeks to the given position in the stream.
+     *
+     * @param int $count The count
+     *
+     * @return void
+     */
+    public function seek(int $count): void
     {
         $this->nextChar += $count;
         $this->currentChar = $this->nextChar;
     }
 
-    public function rewind()
+    /**
+     * Rewinds the steam to the beginning.
+     *
+     * @return void
+     */
+    public function rewind(): void
     {
         $this->currentChar = 0;
     }
