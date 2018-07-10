95f590d488a8e48c922e05a0bfb6b451be5ece92
Trevor Rowbotham <smokey101stair@gmail.com>
2018-07-09 21:18:50 -0400

    Add some type hints to NodeFilterUtils

--- src/NodeFilterUtils.php
+++ src/NodeFilterUtils.php
@@ -1,6 +1,8 @@
 <?php
 namespace Rowbot\DOM;
 
+use function call_user_func;
+
 trait NodeFilterUtils
 {
     /**
@@ -10,14 +12,14 @@ trait NodeFilterUtils
      *
      * @see https://dom.spec.whatwg.org/#concept-node-filter
      *
-     * @param Node $node The node to check.
+     * @param \Rowbot\DOM\Node $node The node to check.
      *
      * @return int Returns one of NodeFilter's FILTER_* constants.
      *     - NodeFilter::FILTER_ACCEPT
      *     - NodeFilter::FILTER_REJECT
      *     - NodeFilter::FILTER_SKIP
      */
-    private function filterNode($node)
+    private function filterNode(Node $node): int
     {
         // Let n be node’s nodeType attribute value minus 1.
         $n = $node->nodeType - 1;
@@ -37,6 +39,6 @@ trait NodeFilterUtils
             return $this->filter->acceptNode($node);
         }
 
-        return \call_user_func($this->filter, $node);
+        return call_user_func($this->filter, $node);
     }
 }
