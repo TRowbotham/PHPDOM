02649849ca52322106f60e4b025636b8513bf015
Trevor Rowbotham <smokey101stair@gmail.com>
2018-07-17 14:10:34 -0400

    Remove some unused code

--- src/Utils.php
+++ src/Utils.php
@@ -3,14 +3,11 @@ declare(strict_types=1);
 
 namespace Rowbot\DOM;
 
-use function intval;
 use function mb_strlen;
 use function mb_substr;
 use function mb_strtolower;
 use function mb_strtoupper;
 use function pow;
-use function preg_match;
-use function strlen;
 
 final class Utils
 {
@@ -100,139 +97,6 @@ final class Utils
         }
 
         return $normalizedOffset;
-    }
-
-    /**
-     * @see https://html.spec.whatwg.org/#rules-for-parsing-floating-point-number-values
-     *
-     * @param string $input
-     *
-     * @return float|false
-     */
-    public static function parseFloatingPointNumber(string $input)
-    {
-        $position = 0;
-        $value = 1;
-        $divisor = 1;
-        $exponent = 1;
-        $length = strlen($input);
-
-        self::collectCodePointSequence($input, $position, '/\s/');
-
-        if ($position >= $length) {
-            return false;
-        }
-
-        if ($input[$position] == '-') {
-            $value = -1;
-            $divisor = -1;
-
-            if (++$position >= $length) {
-                return false;
-            }
-        } elseif ($input[$position] == '+') {
-            if (++$position >= $length) {
-                return false;
-            }
-        }
-
-        if ($input[$position] == '.' && ($position + 1) < ($length - 1) &&
-            preg_match('/\d/', $input[$position + 1])) {
-            $value = 0;
-            goto Fraction;
-        }
-
-        if (!preg_match('/\d/', $input[$position])) {
-            return false;
-        }
-
-        $value *= intval(
-            self::collectCodePointSequence(
-                $input,
-                $position,
-                '/\d/'
-            )
-        );
-
-        if ($position >= $length) {
-            goto Conversion;
-        }
-
-        Fraction:
-        if ($input[$position] == '.') {
-            // TODO: Finish
-        }
-
-        Conversion:
-    }
-
-    /**
-     * @see https://html.spec.whatwg.org/#rules-for-parsing-integers
-     *
-     * @param string $input
-     *
-     * @return int|false
-     */
-    public static function parseSignedInt(string $input)
-    {
-        $position = 0;
-        $sign = 'positive';
-        $length = strlen($input);
-
-        self::collectCodePointSequence($input, $position, '/\s/');
-
-        if ($position >= $length) {
-            return false;
-        }
-
-        if ($input[$position] == '-') {
-            $sign = 'negative';
-
-            if (++$position >= $length) {
-                return false;
-            }
-        } elseif ($input[$position] == '+') {
-            if (++$position >= $length) {
-                return false;
-            }
-        }
-
-        if (!preg_match('/\d/', $input[$position])) {
-            return false;
-        }
-
-        $value = intval(
-            self::collectCodePointSequence(
-                $input,
-                $position,
-                '/\d/'
-            ),
-            10
-        );
-
-        return $sign == 'positive' ? $value : 0 - $value;
-    }
-
-    /**
-     * @see https://html.spec.whatwg.org/#rules-for-parsing-non-negative-integers
-     *
-     * @param string $input
-     *
-     * @return int|false
-     */
-    public static function parseNonNegativeInt(string $input)
-    {
-        $value = self::parseSignedInt($input);
-
-        if ($value === false) {
-            return false;
-        }
-
-        if ($value < 0) {
-            return false;
-        }
-
-        return $value;
     }
 
     /**
