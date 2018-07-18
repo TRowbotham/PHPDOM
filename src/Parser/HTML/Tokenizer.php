9920517e42573045ef93f2257eeb355b98fa49f9
Trevor Rowbotham <smokey101stair@gmail.com>
2018-07-18 19:50:36 -0400

    Update the after attribute value quoted state

--- src/Parser/HTML/Tokenizer.php
+++ src/Parser/HTML/Tokenizer.php
@@ -1759,10 +1759,9 @@ class Tokenizer
                         yield $tagToken;
                     } elseif ($this->inputStream->isEoS()) {
                         // Parse error.
-                        // Reconsume in the data state.
-                        $this->inputStream->seek(-1);
-                        $this->state->tokenizerState =
-                            TokenizerState::DATA;
+                        // Emit an end-of-file token.
+                        yield new EOFToken();
+                        return;
                     } else {
                         // Parse error.
                         // Reconsume in the before attribute name state.
