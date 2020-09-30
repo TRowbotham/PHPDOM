08d9b2e97fb875e6293105f69bc89d2b3703704f
Trevor Rowbotham <trevor.rowbotham@pm.me>
2020-09-30 19:26:43 -0400

    Add some missing obsolete HTML elements

---
+++ src/Element/HTML/HTMLDirectoryElement.php
@@ -0,0 +1,13 @@
+<?php
+namespace Rowbot\DOM\Element\HTML;
+
+/**
+ * @see https://html.spec.whatwg.org/multipage/obsolete.html#htmldirectoryelement
+ */
+class HTMLDirectoryElement extends HTMLElement
+{
+    protected function __construct()
+    {
+        parent::__construct();
+    }
+}
