2d67d789616f572564219f23e70c81dbfd7d2ee9
Trevor Rowbotham <trevor.rowbotham@pm.me>
2021-03-14 19:13:01 -0400

    SVGTextElement should not be abstract

--- src/Element/SVG/SVGTextElement.php
+++ src/Element/SVG/SVGTextElement.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Rowbot\DOM\Element\SVG;
 
 /**
- * @see https://svgwg.org/svg2-draft/text.html#InterfaceSVGTextPositioningElement
+ * @see https://svgwg.org/svg2-draft/text.html#InterfaceSVGTextElement
  */
-abstract class SVGTextElement extends SVGTextPositioningElement
+class SVGTextElement extends SVGTextPositioningElement
 {
 }
