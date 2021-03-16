9906254f3e985ba5ee51b43d52b9fac92788ccbc
Trevor Rowbotham <trevor.rowbotham@pm.me>
2021-03-16 15:17:19 -0400

    Fix XML fragment parsing algorithm
    Wrap the input in the the context element's tag. Still need to handle
    namespaces. Also, return the children of the document element and not
    the document.

--- src/Parser/XML/XMLParser.php
+++ src/Parser/XML/XMLParser.php
@@ -38,26 +38,49 @@ class XMLParser extends Parser
      */
     public static function parseXMLFragment(string $input, Element $contextElement): NodeList
     {
+        // 1. Create a new XML parser.
         $document = new Document();
         $parser = new XMLParser($document);
         $parser->inputStream->append($input);
 
+        // 2. Feed the parser just created the string corresponding to the start tag of the context element, declaring
+        // all the namespace prefixes that are in scope on that element in the DOM, as well as declaring the default
+        // namespace (if any) that is in scope on that element in the DOM.
+
+        // 3. Feed the parser just created the string input.
+
+        // 4. Feed the parser just created the string corresponding to the end tag of the context element.
+
+        // A namespace prefix is in scope if the DOM lookupNamespaceURI() method on the element would return a non-null
+        // value for that prefix.
+
+        // The default namespace is the namespace for which the DOM isDefaultNamespace() method on the element would
+        // return true.
+
+        // Note: No DOCTYPE is passed to the parser, and therefore no external subset is referenced, and therefore no
+        // entities will be recognized.
+        $tagName = $contextElement->tagName;
+        // TODO: Declare all the namespace prefixes that are in scope
+        $parser->inputStream->append("<{$tagName}>{$input}</{$tagName}>");
+
+        // 5. If there is an XML well-formedness or XML namespace well-formedness error, then throw a
+        // "SyntaxError" DOMException.
         try {
             $parser->run();
         } catch (Throwable $e) {
             throw new SyntaxError('', $e);
         }
 
+        // 6. If the document element of the resulting Document has any sibling nodes, then throw a
+        // "SyntaxError" DOMException.
         $docElement = $document->documentElement;
 
-        if (
-            $docElement !== null
-            && ($docElement->previousSibling !== null || $docElement->nextSibling !== null)
-        ) {
+        if ($docElement !== null && ($docElement->previousSibling !== null || $docElement->nextSibling !== null)) {
             throw new SyntaxError();
         }
 
-        return $document->childNodes;
+        // 7. Return the child nodes of the document element of the resulting Document, in tree order.
+        return $docElement->childNodes;
     }
 
     public function preprocessInputStream(string $input): void
