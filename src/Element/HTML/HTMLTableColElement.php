3d253a3fe794d33475b335c0b943dfbe2edea960
Trevor Rowbotham <trevor.rowbotham@pm.me>
2024-03-28 02:46:05 -0400

    Convert HTMLTableColElement to use hooks

--- src/Element/HTML/HTMLTableColElement.php
+++ src/Element/HTML/HTMLTableColElement.php
@@ -4,24 +4,19 @@ declare(strict_types=1);
 
 namespace Rowbot\DOM\Element\HTML;
 
-use Rowbot\DOM\DynamicProperty\Getter;
-use Rowbot\DOM\DynamicProperty\Setter;
-
 /**
  * @see https://html.spec.whatwg.org/multipage/tables.html#the-colgroup-element
  * @see https://html.spec.whatwg.org/multipage/tables.html#the-col-element
  */
 class HTMLTableColElement extends HTMLElement
 {
-    #[Getter('span')]
-    private function getSpan(): int
-    {
-        return $this->reflectClampedUnsignedLongAttributeValue('span', 1, 1000, 1);
-    }
-
-    #[Setter('span')]
-    private function setSpan(mixed $value): void
-    {
-        $this->setLongAttributeValue('span', $value, self::UNSIGNED_LONG);
+    /**
+     * @see https://html.spec.whatwg.org/multipage/tables.html#dom-colgroup-span
+     */
+    public int $span {
+        get => $this->reflectClampedUnsignedLongAttributeValue('span', 1, 1000, 1);
+        set {
+            $this->setLongAttributeValue('span', $value, self::UNSIGNED_LONG);
+        }
     }
 }
