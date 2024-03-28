6fb5a708a10017be42bb3d62a9e03690548ea468
Trevor Rowbotham <trevor.rowbotham@pm.me>
2024-03-28 02:12:50 -0400

    Convert HTMLImageElement to use hooks

--- src/Element/HTML/HTMLImageElement.php
+++ src/Element/HTML/HTMLImageElement.php
@@ -4,29 +4,18 @@ declare(strict_types=1);
 
 namespace Rowbot\DOM\Element\HTML;
 
-use Rowbot\DOM\DynamicProperty\Getter;
-use Rowbot\DOM\DynamicProperty\Setter;
-use Rowbot\DOM\Exception\TypeError;
-use Rowbot\DOM\Utils;
-
 /**
  * @see https://html.spec.whatwg.org/multipage/embedded-content.html#the-img-element
  */
 class HTMLImageElement extends HTMLElement
 {
-    #[Getter('src')]
-    private function getSrc(): string
-    {
-        return $this->reflectUrlAttribute('src');
-    }
-
-    #[Setter('src')]
-    private function setSrc(mixed $value): void
-    {
-        if (!Utils::isStringable($value)) {
-            throw new TypeError();
+    /**
+     * @see https://html.spec.whatwg.org/multipage/embedded-content.html#attr-img-src
+     */
+    public string $src {
+        get => $this->reflectUrlAttribute('src');
+        set {
+            $this->attributeList->setAttrValue('src', $value);
         }
-
-        $this->attributeList->setAttrValue('src', (string) $value);
     }
 }
