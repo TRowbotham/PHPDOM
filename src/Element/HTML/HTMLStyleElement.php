97b72ab23b69bfebfdd0fd48e773a7265c82b6f3
Trevor Rowbotham <trevor.rowbotham@pm.me>
2024-03-28 02:29:37 -0400

    Convert HTMLStyleElement to use hooks

--- src/Element/HTML/HTMLStyleElement.php
+++ src/Element/HTML/HTMLStyleElement.php
@@ -4,52 +4,30 @@ declare(strict_types=1);
 
 namespace Rowbot\DOM\Element\HTML;
 
-use Rowbot\DOM\DynamicProperty\Getter;
-use Rowbot\DOM\DynamicProperty\Setter;
-use Rowbot\DOM\Exception\TypeError;
-use Rowbot\DOM\Utils;
-
 /**
  * Represents the HTML <style> element.
  *
  * @see https://html.spec.whatwg.org/multipage/semantics.html#the-style-element
- *
- * @property string $media  Reflects the HTML media attribute. This accepts a valid media query to instruct the browser
- *                          on when this resource should apply to the document.
- * @property string $type   Reflects the HTML type attribute, which hints to the browser what the content's MIME type
- *                          is. This property defaults to text/css.
  */
 class HTMLStyleElement extends HTMLElement
 {
-    #[Getter('media')]
-    private function getMedia(): string
-    {
-        return $this->reflectStringAttributeValue('media');
-    }
-
-    #[Getter('type')]
-    private function getType(): string
-    {
-        return $this->reflectStringAttributeValue('type');
-    }
-
-    #[Setter('media')]
-    private function setMedia(mixed $value): void
-    {
-        if (!Utils::isStringable($value)) {
-            throw new TypeError();
+    /**
+     * @see https://html.spec.whatwg.org/multipage/semantics.html#dom-style-media
+     */
+    public string $media {
+        get => $this->reflectStringAttributeValue('media');
+        set {
+            $this->attributeList->setAttrValue('media', $value);
         }
-
-        $this->attributeList->setAttrValue('media', (string) $value);
     }
 
-    #[Setter('type')]
-    private function setType(mixed $value): void
-    {
-        if (!Utils::isStringable($value)) {
-            throw new TypeError();
+    /**
+     * @see https://html.spec.whatwg.org/multipage/obsolete.html#dom-style-type
+     */
+    public string $type {
+        get => $this->reflectStringAttributeValue('type');
+        set {
+            $this->attributeList->setAttrValue('type', $value);
         }
-
-        $this->attributeList->setAttrValue('type', (string) $value);
     }
 }
